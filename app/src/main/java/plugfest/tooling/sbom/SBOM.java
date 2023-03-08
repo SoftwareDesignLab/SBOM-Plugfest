483214763e1609202b7e853fad6c72447655a3bb
txdvse <txdvse@rit.edu>
2023-03-08 16:05:40 -0500

    SBOM.java + implementation for basic sbom object contains an sbom's information

--- app/src/main/java/plugfest/tooling/sbom/SBOM.java
+++ app/src/main/java/plugfest/tooling/sbom/SBOM.java
@@ -1,4 +1,36 @@
 package plugfest.tooling.sbom;
 
+import com.google.common.collect.Multimap;
+
+import java.util.HashMap;
+
+/**
+ * File: SBOM.java
+ * Hold's relevant data from a submitted SBOM
+ *
+ * @author Tyler Drake
+ */
 public class SBOM {
+
+    Multimap<String, String> header;
+
+    HashMap<String, Component> components;
+
+    Multimap<String, String> relationships;
+
+    public SBOM() {
+    }
+
+    public void addToHeader(String key, String value) {
+        header.put(key, value);
+    }
+
+    public void addComponent(String key, Component component) {
+        components.put(key, component);
+    }
+
+    public void addRelationship(String key, String value) {
+        relationships.put(key, value);
+    }
+
 }
