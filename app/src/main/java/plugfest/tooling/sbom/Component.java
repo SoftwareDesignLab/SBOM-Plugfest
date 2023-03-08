f59edec5671b50388897ff329708fcd2cf0f0c77
txdvse <txdvse@rit.edu>
2023-03-08 16:08:47 -0500

    Component.java + Added basic component implementation that holds information

--- app/src/main/java/plugfest/tooling/sbom/Component.java
+++ app/src/main/java/plugfest/tooling/sbom/Component.java
@@ -1,4 +1,22 @@
 package plugfest.tooling.sbom;
 
+import com.google.common.collect.Multimap;
+
+/**
+ * File: Component.java
+ * Hold's relevant data for a component in an SBOM
+ *
+ * @author Tyler Drake
+ */
 public class Component {
+
+    Multimap<String, String> component_information;
+
+    public Component() {
+    }
+
+    public void addInformation(String key, String value) {
+        component_information.put(key, value);
+    }
+
 }
