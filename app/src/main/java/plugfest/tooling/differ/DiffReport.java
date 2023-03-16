9111945a015c72ff7f84734c08ce716e0183b361
pxlvse <pxlvse@rit.edu>
2023-03-16 12:25:32 -0400

    init

--- app/src/main/java/plugfest/tooling/differ/DiffReport.java
+++ app/src/main/java/plugfest/tooling/differ/DiffReport.java
@@ -1,4 +1,57 @@
 package plugfest.tooling.differ;
 
+
+import java.util.ArrayList;
+
+
 public class DiffReport {
+    
+    private static String dfn = "NONAME", cfn = "NONAME";
+    private static final ArrayList<Object> replist = new ArrayList<Object>();
+    private String  linenum = "-1";
+    private String  dlistcontent = "";
+    private String  clistcontent = "";
+    
+    public DiffReport(){};
+    public DiffReport(Object ln, Object dc, Object cc){
+           this.linenum = ln.toString();
+           this.dlistcontent = dc.toString();
+           this.clistcontent = cc.toString();
+    };
+    
+    public void append(Object s){
+        replist.add(s);
+    }
+    
+    private void printHeader(){
+        System.out.println("< " + this.dfn + " : " + "> " + this.cfn + "\n-----");
+    }
+    
+    private void printHeader(String dfn, String cfn){
+        System.out.println("< " + dfn + " : " + "> " + cfn + "\n-----");
+    }
+    
+    public void print(){
+        if(! this.dfn.equals("NONAME")) {
+           this.printHeader();
+        }
+        for(Object i : replist)
+        {
+            System.out.println(i);
+        }
+    }
+    
+    public void setFn(String dfn, String cfn){
+        this.dfn = dfn;
+        this.cfn = cfn;
+    }
+    
+   // Overriding toString() method of String class
+    @Override
+    public String toString() {
+        return this.linenum  + "\n=====\n" + 
+               "< " + this.dlistcontent + "\n" +
+               "> " + this.clistcontent + "\n";
+    }
 }
+
--- app/src/main/java/plugfest/tooling/differ/FullDiff.java
+++ app/src/main/java/plugfest/tooling/differ/FullDiff.java
@@ -1,4 +1,68 @@
 package plugfest.tooling.differ;
 
+import java.util.ArrayList;
+
+
 public class FullDiff {
+    private static Long maxSize= 100000L;
+    
+    private ReadFile d = null; 
+    private ReadFile c = null;
+    private DiffReport report = new DiffReport();
+    
+    public FullDiff(){};
+    
+    public FullDiff(ReadFile d, ReadFile c){
+         this.d = d;
+         this.c = c;
+    }
+    
+    public DiffReport diff()
+    {
+        ArrayList<Object> dd = this.d.getdata();
+        ArrayList<Object> cc = this.c.getdata();
+        int dsize = dd.size();
+        int csize = cc.size();
+        int c; // = (dsize < csize) ? dsize: csize;
+        
+        if(dsize < csize){
+            c = dsize;
+            this.report.setFn(this.d.getfilename(), this.c.getfilename());
+        }
+        else {
+            c = csize;
+            this.report.setFn(this.c.getfilename(), this.d.getfilename());
+            dd = this.c.getdata();
+            cc = this.d.getdata();
+        }
+        
+        //check file size????????
+//        if(dsize != csize) {
+//          report.append(new DiffReport( 0,"file 1's size is not the same as file 2",
+//                                             "file 2's size is not the same as file 1"));
+//        }
+        
+        for(int i=0; i<c; i++)
+        {
+            if( ! dd.get(i).toString().equals(cc.get(i).toString())) {
+                //finding the position of the first diff char
+                
+                //if c > maxsize:
+                    //printout header
+                    //output result to console 
+                //else
+                report.append(new DiffReport( i+1, dd.get(i), cc.get(i)));
+            }               
+        }
+        
+        //output the rest from the longer file:
+           //stdout or report.append();
+        
+        return report;
+    }
+    
+    public void append(Object s){
+        report.append(s);
+    }
 }
+
