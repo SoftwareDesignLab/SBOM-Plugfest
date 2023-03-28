bfc9f5f4da1b036da60631a40210b2546190f50d
Matt London <mattlondon57@gmail.com>
2023-03-28 17:02:54 -0400

    Report containing both sbom conflicts and component conflicts

--- app/src/main/java/plugfest/tooling/differ/DiffReport.java
+++ app/src/main/java/plugfest/tooling/differ/DiffReport.java
@@ -1,5 +1,35 @@
 package plugfest.tooling.differ;
 
-public class DiffReport {
+import plugfest.tooling.sbom.ComponentConflict;
+import plugfest.tooling.sbom.SBOMConflict;
 
+import java.util.Set;
+
+public class DiffReport {
+    private SBOMConflict sbomConflict;
+    private Set<ComponentConflict> componentConflicts;
+
+    public DiffReport(SBOMConflict sbomConflict, Set<ComponentConflict> componentConflicts) {
+        this.sbomConflict = sbomConflict;
+        this.componentConflicts = componentConflicts;
+    }
+
+    public SBOMConflict getSbomConflict() {
+        return sbomConflict;
+    }
+
+    public Set<ComponentConflict> getComponentConflicts() {
+        return componentConflicts;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(sbomConflict.toString());
+        sb.append("Component Conflicts:\n");
+        for (ComponentConflict conflict : componentConflicts) {
+            sb.append(conflict.toString());
+        }
+
+        return sb.toString();
+    }
 }
