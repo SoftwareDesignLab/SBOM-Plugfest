7f682f9c73aea5cd0f37574c761461eb3a69855a
ftqo <brian@ftqo.dev>
2023-03-28 16:35:36 -0400

    add addScore method to Metric

--- app/src/main/java/plugfest/tooling/metrics/Metric.java
+++ app/src/main/java/plugfest/tooling/metrics/Metric.java
@@ -2,6 +2,11 @@ package plugfest.tooling.metrics;
 
 public abstract class Metric {
     private int score;
-    public int getScore() { return this.score; }
+    public int getScore() {
+        return this.score;
+    }
+    protected void addScore(int score) {
+        this.score += score;
+    }
     protected abstract int testMetric();
 }
