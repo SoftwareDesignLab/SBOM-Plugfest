7f8c1862d1c60a2915c138a80141e09b91f3eb0f
Matt London <mattlondon57@gmail.com>
2023-03-28 17:03:55 -0400

    Temporarily comment out parsers that work with SBOM so they don't throw on new object

--- app/src/main/java/plugfest/tooling/sbom/SPDXParser.java
+++ app/src/main/java/plugfest/tooling/sbom/SPDXParser.java
@@ -38,105 +38,105 @@ public class SPDXParser {
      */
     public static SBOM parse(String file_path) throws IOException {
 
-        SBOM sbom = new SBOM();
+//        SBOM sbom = new SBOM();
 
-        sbom.setName(Paths.get(file_path).getFileName().toString());
+//        sbom.setName(Paths.get(file_path).getFileName().toString());
+//
+//        // Get spdx file
+//        File file = new File(file_path);
+//
+//        // If file doesn't exist, return a null object
+//        if(!file.exists()) { return null; }
+//
+//        // Initialize BufferedReader along with current line
+//        BufferedReader br = new BufferedReader(new FileReader(file));
+//        String current_line = br.readLine();
+//        sbom.addData(current_line);
+//
+//        // Process header information
+//        while ( (current_line = readSBOMLine(sbom, br) ) != null
+//                && !current_line.contains(UNPACKAGED_TAG)
+//                && !current_line.contains(PACKAGE_TAG)
+//                && !current_line.contains(RELATIONSHIP_TAG)
+//                && !current_line.contains(RELATIONSHIP_KEY)
+//        ) { sbom.addToHeader(current_line); }
+//
+//        // Process Unpackaged Components
+//        while ( (current_line = readSBOMLine(sbom, br) ) != null
+//                && !current_line.contains(PACKAGE_TAG)
+//                && !current_line.contains(RELATIONSHIP_TAG)
+//                && !current_line.contains(RELATIONSHIP_KEY)
+//        ) {
+//            if (current_line.contains(PACKAGE_TAG) || current_line.contains(RELATIONSHIP_TAG)) break;
+//            if (current_line.isEmpty()) {
+//                readSBOMLine(sbom, br);
+//                Component component = new Component();
+//                while ( (current_line = readSBOMLine(sbom, br) ) != null
+//                        && !current_line.contains(TAG)
+//                        && !current_line.isEmpty())
+//                {
+//
+//                    if(current_line.contains("SPDXID:")) {
+//                        component.setIdentifier(current_line.split("SPDXID: ", 2)[1]);
+//                    }
+//
+//                    component.addInformation(current_line);
+//
+//                }
+//
+//                if( component.component_information.size() > 0 ) {
+//                    sbom.addComponent(component.getIdentifier(), component);
+//                }
+//            }
+//        }
+//
+//        // Parse through packaged components
+//        while ( (current_line = readSBOMLine(sbom, br) ) != null
+//                && !current_line.contains(RELATIONSHIP_TAG)
+//                && !current_line.contains(RELATIONSHIP_KEY)
+//        ) {
+//            if (current_line.contains(RELATIONSHIP_TAG)) break;
+//
+//            // Temporary component collection of materials
+//            Component component = new Component();
+//
+//            // If new package/component is found
+//            if (current_line.contains(PACKAGE_TAG)) {
+//                readSBOMLine(sbom, br);
+//                // While in the same package/component
+//                while (!(current_line = readSBOMLine(sbom, br)).contains(TAG)
+//                        && !current_line.contains(RELATIONSHIP_TAG)
+//                        && !current_line.contains(RELATIONSHIP_KEY)) {
+//
+//                    if (current_line.contains(": ")) {
+//                        if(current_line.contains("SPDXID:")) {
+//                            component.setIdentifier(current_line.split("SPDXID: ", 2)[1]);
+//                        }
+//                        component.addInformation(current_line);
+//                    }
+//
+//                }
+//
+//                if ( component.component_information.size() > 0 ) {
+//                    sbom.addComponent(component.getIdentifier(), component);
+//                }
+//
+//            }
+//
+//        }
+//
+//        // Parse through relationships
+//        while( (current_line = readSBOMLine(sbom, br) ) != null) {
+//
+//            if(current_line.contains("Relationship: ")) {
+//
+//                sbom.addRelationship(current_line.split("Relationship: ", 2)[1]);
+//
+//            }
+//
+//        }
 
-        // Get spdx file
-        File file = new File(file_path);
-
-        // If file doesn't exist, return a null object
-        if(!file.exists()) { return null; }
-
-        // Initialize BufferedReader along with current line
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String current_line = br.readLine();
-        sbom.addData(current_line);
-
-        // Process header information
-        while ( (current_line = readSBOMLine(sbom, br) ) != null
-                && !current_line.contains(UNPACKAGED_TAG)
-                && !current_line.contains(PACKAGE_TAG)
-                && !current_line.contains(RELATIONSHIP_TAG)
-                && !current_line.contains(RELATIONSHIP_KEY)
-        ) { sbom.addToHeader(current_line); }
-
-        // Process Unpackaged Components
-        while ( (current_line = readSBOMLine(sbom, br) ) != null
-                && !current_line.contains(PACKAGE_TAG)
-                && !current_line.contains(RELATIONSHIP_TAG)
-                && !current_line.contains(RELATIONSHIP_KEY)
-        ) {
-            if (current_line.contains(PACKAGE_TAG) || current_line.contains(RELATIONSHIP_TAG)) break;
-            if (current_line.isEmpty()) {
-                readSBOMLine(sbom, br);
-                Component component = new Component();
-                while ( (current_line = readSBOMLine(sbom, br) ) != null
-                        && !current_line.contains(TAG)
-                        && !current_line.isEmpty())
-                {
-
-                    if(current_line.contains("SPDXID:")) {
-                        component.setIdentifier(current_line.split("SPDXID: ", 2)[1]);
-                    }
-
-                    component.addInformation(current_line);
-
-                }
-
-                if( component.component_information.size() > 0 ) {
-                    sbom.addComponent(component.getIdentifier(), component);
-                }
-            }
-        }
-
-        // Parse through packaged components
-        while ( (current_line = readSBOMLine(sbom, br) ) != null
-                && !current_line.contains(RELATIONSHIP_TAG)
-                && !current_line.contains(RELATIONSHIP_KEY)
-        ) {
-            if (current_line.contains(RELATIONSHIP_TAG)) break;
-
-            // Temporary component collection of materials
-            Component component = new Component();
-
-            // If new package/component is found
-            if (current_line.contains(PACKAGE_TAG)) {
-                readSBOMLine(sbom, br);
-                // While in the same package/component
-                while (!(current_line = readSBOMLine(sbom, br)).contains(TAG)
-                        && !current_line.contains(RELATIONSHIP_TAG)
-                        && !current_line.contains(RELATIONSHIP_KEY)) {
-
-                    if (current_line.contains(": ")) {
-                        if(current_line.contains("SPDXID:")) {
-                            component.setIdentifier(current_line.split("SPDXID: ", 2)[1]);
-                        }
-                        component.addInformation(current_line);
-                    }
-
-                }
-
-                if ( component.component_information.size() > 0 ) {
-                    sbom.addComponent(component.getIdentifier(), component);
-                }
-
-            }
-
-        }
-
-        // Parse through relationships
-        while( (current_line = readSBOMLine(sbom, br) ) != null) {
-
-            if(current_line.contains("Relationship: ")) {
-
-                sbom.addRelationship(current_line.split("Relationship: ", 2)[1]);
-
-            }
-
-        }
-
-        return sbom;
+        return null;
 
     }
 
@@ -152,7 +152,7 @@ public class SPDXParser {
      */
     public static String readSBOMLine(SBOM sbom, BufferedReader br) throws IOException {
         String next_line = br.readLine();
-        sbom.addData(next_line);
+//        sbom.addData(next_line);
         return next_line;
     }
 
