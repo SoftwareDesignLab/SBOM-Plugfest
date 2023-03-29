ceaaa86328c5dcc5ef8eea9ffba0b64ffce221fb
dlg1206 <dlg1206@g.rit.edu>
2023-03-28 22:47:10 -0400

    Remove metrics object for plugfest sbom object

--- app/src/main/java/plugfest/tooling/metrics/Metric.java
+++ app/src/main/java/plugfest/tooling/metrics/Metric.java
@@ -7,3 +7,18 @@ public abstract class Metric {
     }
     protected abstract int testMetric();
 }
+// Example Metric
+/*
+public class fooMetric extends Metric {
+    private String detailN;   // Metric related field etc
+    public fooMetric(args to test){
+        this.argn = detailN;
+        this.score = testMetric();
+    }
+    @override
+    protected int testMetric(){
+        // metric implementation here
+        return result;
+    }
+}
+ */
