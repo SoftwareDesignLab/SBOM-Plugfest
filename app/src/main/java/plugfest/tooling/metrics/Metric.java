684e3a95235615bb4cf1a60313b697349cba726c
dlg1206 <dlg1206@g.rit.edu>
2023-03-28 22:55:58 -0400

    add example metric to qa pipeline

--- app/src/main/java/plugfest/tooling/metrics/Metric.java
+++ app/src/main/java/plugfest/tooling/metrics/Metric.java
@@ -1,5 +1,8 @@
 package plugfest.tooling.metrics;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public abstract class Metric {
     protected int score;
     public int getScore() {
@@ -7,18 +10,3 @@ public abstract class Metric {
     }
     protected abstract int testMetric();
 }
-// Example Metric
-/*
-public class fooMetric extends Metric {
-    private String detailN;   // Metric related field etc
-    public fooMetric(args to test){
-        this.argn = detailN;
-        this.score = testMetric();
-    }
-    @override
-    protected int testMetric(){
-        // metric implementation here
-        return result;
-    }
-}
- */
--- app/src/main/java/plugfest/tooling/qa/QAPipeline.java
+++ app/src/main/java/plugfest/tooling/qa/QAPipeline.java
@@ -1,4 +1,17 @@
 package plugfest.tooling.qa;
 
+import plugfest.tooling.sbom.SBOM;
+
 public class QAPipeline {
+
+
+
+    public QAPipeline(){
+
+    }
+
+
+    public void process(SBOM sbom){
+        sbom.getTimestamp();
+    }
 }
