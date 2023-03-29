b13d847f1ba00c6cab96e2e712a3950ed9492352
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-03-29 18:47:00 -0400

    Added regex expressions

--- app/src/main/java/plugfest/tooling/qa/processors/ContextualProcessor.java
+++ app/src/main/java/plugfest/tooling/qa/processors/ContextualProcessor.java
@@ -18,19 +18,22 @@ public class ContextualProcessor implements AttributeProcessor{
         final String UUIDShort = c.getUUID().toString().substring(0, 5);
 
         // Check accuracy of supplier name
-        // TODO: Regex
-        if(!Pattern.compile("componentPublisherRegex").matcher(c.getPublisher()).matches())
+        // Regex101: https://regex101.com/r/KNxGCb/3
+        // Checks if name is in form: "Person: First Last <email@mail.com>"
+        if(!Pattern.compile("^Person: ([\\w äöüÄÖÜß]*) <(.*)>").matcher(c.getPublisher().strip()).matches())
             testResult.append(String.format("FAILED: Component %s Publisher Name is Not Complete", UUIDShort));
-        // Check accuracy of component version
-        // TODO: Regex
-        if(!Pattern.compile("componentNameRegex").matcher(c.getName()).matches()) {
+
+        // Check accuracy of component name
+        if(c.getName().isBlank()) {
             // Add separator if not first check to fail
             if(!testResult.isEmpty()) testResult.append("\n");
             testResult.append(String.format("FAILED: Component %s Name is Not Complete", UUIDShort));
         }
-        // Check accuracy of component name
-        // TODO: Regex
-        if(!Pattern.compile("componentVersionRegex").matcher(c.getVersion()).matches()) {
+
+        // Check accuracy of component version
+        // Regex101: https://regex101.com/r/wzJeIq/1
+        // Checks if version is in form: "1.*" | "1:*", version format varies a lot
+        if(!Pattern.compile("^([0-9](?:.|:).*)").matcher(c.getVersion().strip()).matches()) {
             // Add separator if not first check to fail
             if(!testResult.isEmpty()) testResult.append("\n");
             testResult.append(String.format("FAILED: Component %s Version is Not Complete", UUIDShort));
