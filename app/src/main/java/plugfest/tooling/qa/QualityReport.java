f6d69d6ed6ef5e5b10724cf3af15cd8660c755c8
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-03-29 17:56:04 -0400

    Updated to store test results instead of score

--- app/src/main/java/plugfest/tooling/qa/QualityReport.java
+++ app/src/main/java/plugfest/tooling/qa/QualityReport.java
@@ -1,16 +1,22 @@
 package plugfest.tooling.qa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QualityReport {
-    private int score = 0;
+    private final Map<String, String> testResults;
+
     // include data on what tests were run
     public QualityReport(){
-
+        testResults = new HashMap<>();
     }
 
-    public void updateScore(int increment) {
-        this.score += increment;
-    }
-    public int getScore() {
-        return this.score;
+    public void addTestResult(String testName, String testResult) {
+        // Insert the new test result
+        final String existingValue = this.testResults.put(testName, testResult);
+
+        // If existingValue is not null, this action overrode that value
+        if(existingValue != null)
+            System.out.printf("Test %s already existed with value %s and has been overridden with value %s.", testName, existingValue, testResult);
     }
 }
