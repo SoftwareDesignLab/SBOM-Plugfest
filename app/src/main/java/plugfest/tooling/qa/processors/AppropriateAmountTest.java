5feb213cdae717ea27a590dc6638c90bbaf7ec2b
Ian Dunn <techwizguy7@gmail.com>
2023-03-30 15:08:30 -0400

    Create test for Appropriate Amount DQ

---
+++ app/src/main/java/plugfest/tooling/qa/processors/AppropriateAmountTest.java
@@ -0,0 +1,73 @@
+package plugfest.tooling.qa.processors;
+
+import plugfest.tooling.sbom.Component;
+
+import java.util.ArrayList;
+
+/**
+ * File: AppropriateAmountTest.java
+ * Tests a component to check if each attribute is <= 80 characters in length, the limit for max line length in Java.
+ *
+ * @author Ian Dunn
+ */
+public class AppropriateAmountTest extends MetricTest {
+
+    /**
+     * Current maximum line length each attribute is being tested on.
+     */
+    private final int maxLineLength;
+
+    /**
+     * Create new ApproprateAmountTest and specify maximum line length.
+     */
+    protected AppropriateAmountTest() {
+        super("Appropriate Amount Test");
+
+        // Set maximum line length to 80
+        this.maxLineLength = 80;
+    }
+
+    /**
+     * Test a component to check its attributes' maximum line lengths.
+     *
+     * @param c The component to test
+     * @return ArrayList of all generated test results
+     */
+    @Override
+    public ArrayList<String> test(Component c) {
+        // Init StringBuilder
+        final ArrayList<String> testResults = new ArrayList<>();
+        final String UUIDShort = c.getUUID().toString().substring(0, 5);
+
+        /*
+            Component publisher name length <= 80 chars
+         */
+        if(c.getPublisher().strip().length() > maxLineLength) {
+            testResults.add(String.format("FAILED: Component %s Publisher Name Length > 80", UUIDShort));
+        }
+
+        /*
+            Component name length <= 80 chars
+         */
+        if(c.getName().strip().length() > maxLineLength) {
+            // Add separator if not first check to fail
+            if(!testResults.isEmpty()) testResults.add("\n");
+            testResults.add(String.format("FAILED: Component %s Name Length > 80", UUIDShort));
+        }
+
+        /*
+            Component version length <= 80 chars
+         */
+        if(c.getVersion().strip().length() > maxLineLength) {
+            // Add separator if not first check to fail
+            if(!testResults.isEmpty()) testResults.add("\n");
+            testResults.add(String.format("FAILED: Component %s Version Length > 80", UUIDShort));
+        }
+
+        // If no checks failed, mark test as passed
+        if(testResults.isEmpty()) testResults.add("PASSED");
+
+        // Return result
+        return testResults;
+    }
+}
--- app/src/main/java/plugfest/tooling/qa/processors/ContextualProcessor.java
+++ app/src/main/java/plugfest/tooling/qa/processors/ContextualProcessor.java
@@ -3,7 +3,8 @@ package plugfest.tooling.qa.processors;
 public class ContextualProcessor extends AttributeProcessor {
     public ContextualProcessor() {
         super(new MetricTest[]{
-                new CompletenessTest()
+                new CompletenessTest(),
+                new AppropriateAmountTest()
                 // Add new tests here
         });
     }
