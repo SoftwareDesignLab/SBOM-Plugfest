366323fa041bf022df3e34df9d19af8e2bd0ff96
hkeena98 <hkeena98@gmail.com>
2023-03-30 15:14:29 -0400

    Added Verification for SPDXProcessor

--- app/src/main/java/plugfest/tooling/qa/processors/SPDXProcessor.java
+++ app/src/main/java/plugfest/tooling/qa/processors/SPDXProcessor.java
@@ -1,5 +1,18 @@
 package plugfest.tooling.qa.processors;
 
+/**
+ * Imports SPDX Tools
+ */
+import org.spdx.tools.CompareSpdxDocs;
+import org.spdx.tools.Verify;
+import org.spdx.tools.SpdxToolsHelper;
+
+/**
+ * Imports Java Native Libraries
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SPDXProcessor extends SchemaProcessor{
     /*
     todo
@@ -7,4 +20,27 @@ public class SPDXProcessor extends SchemaProcessor{
     File conforms to schema
     Any schema specific tests
      */
+    public ArrayList<String> testVerifySPDX(String filePath) {
+        System.out.println("Running Verification on SPDX SBOM File: " + filePath);
+        ArrayList<String> verificationResults = new ArrayList<>();
+        final String extn = filePath.substring(filePath.indexOf('.'));
+        try {
+            switch (extn) {
+                case "json" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.JSON);
+                case "rdf.xml", "rdf" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.RDFXML);
+                case "xml" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.XML);
+                case "xls" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.XLS);
+                case "xlsx" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.XLSX);
+                case "yaml" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.YAML);
+                case "tag", "spdx" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.TAG);
+                case "rdf.ttl" -> verificationResults = (ArrayList<String>)Verify.verify(filePath, SpdxToolsHelper.SerFileType.RDFTTL);
+            }
+        }
+        catch(Exception ex) {
+            System.out.println("EXCEPTION: "+ex);
+            return null;
+        }
+        return verificationResults;
+    }
+
 }
