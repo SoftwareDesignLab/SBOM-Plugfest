4de40bfc1cd1f3ccba621d0d98af9c84328881de
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-03-30 14:26:50 -0400

    Moved regex pattern compilation to Constructor

--- app/src/main/java/plugfest/tooling/qa/processors/CompletenessTest.java
+++ app/src/main/java/plugfest/tooling/qa/processors/CompletenessTest.java
@@ -6,32 +6,39 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class CompletenessTest extends MetricTest {
+    private final Pattern supplierNameRegex;
+    private final Pattern componentVersionRegex;
+
     protected CompletenessTest() {
         super("Completeness Test");
+        // Regex101: https://regex101.com/r/KNxGCb/3
+        // Checks if name is in form: "Person: First Last <email@mail.com>"
+        this.supplierNameRegex = Pattern.compile("^Person: ([\\w äöüÄÖÜß]*) <(.*)>", Pattern.MULTILINE);
+
+        // Regex101: https://regex101.com/r/wzJeIq/2
+        // Checks if version is in form: "1.*" | "1:*", version format varies a lot
+        this.componentVersionRegex = Pattern.compile("^([0-9]+(?:\\.|:).*)", Pattern.MULTILINE);
     }
+
     @Override
     public ArrayList<String> test(Component c){
         // Init StringBuilder
         final ArrayList<String> testResults = new ArrayList<>();
         final String UUIDShort = c.getUUID().toString().substring(0, 5);
 
-        // Check accuracy of supplier name
-        // Regex101: https://regex101.com/r/KNxGCb/3
-        // Checks if name is in form: "Person: First Last <email@mail.com>"
-        if(!Pattern.compile("^Person: ([\\w äöüÄÖÜß]*) <(.*)>", Pattern.MULTILINE).matcher(c.getPublisher().strip()).matches())
+        // Check completeness of supplier name
+        if(!this.supplierNameRegex.matcher(c.getPublisher().strip()).matches())
             testResults.add(String.format("FAILED: Component %s Publisher Name is Not Complete", UUIDShort));
 
-        // Check accuracy of component name
+        // Check completeness of component name
         if(c.getName().isBlank()) {
             // Add separator if not first check to fail
             if(!testResults.isEmpty()) testResults.add("\n");
             testResults.add(String.format("FAILED: Component %s Name is Not Complete", UUIDShort));
         }
 
-        // Check accuracy of component version
-        // Regex101: https://regex101.com/r/wzJeIq/2
-        // Checks if version is in form: "1.*" | "1:*", version format varies a lot
-        if(!Pattern.compile("^([0-9]+(?:\\.|:).*)", Pattern.MULTILINE).matcher(c.getVersion().strip()).matches()) {
+        // Check completeness of component version
+        if(!this.componentVersionRegex.matcher(c.getVersion().strip()).matches()) {
             // Add separator if not first check to fail
             if(!testResults.isEmpty()) testResults.add("\n");
             testResults.add(String.format("FAILED: Component %s Version is Not Complete", UUIDShort));
