64b5c0a74fd349b0da23adf2aec53e26cb7af80c
txdvse <txdvse@rit.edu>
2023-04-05 03:48:48 -0400

    TranslatorCDXJSON.java + Skeleton file for new translator

---
+++ app/src/main/java/plugfest/tooling/translator/TranslatorCDXJSON.java
@@ -0,0 +1,14 @@
+package plugfest.tooling.translator;
+
+import plugfest.tooling.sbom.SBOM;
+
+public class TranslatorCDXJSON {
+
+    public static SBOM translatorCDXJSON(String file_path) {
+
+        SBOM sbom = null;
+
+        return sbom;
+    }
+
+}
