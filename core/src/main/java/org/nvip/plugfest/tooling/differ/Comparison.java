9342089aac1efec1468fd1dac0a4848d5f4ac809
txdvse <txdvse@rit.edu>
2023-04-12 10:27:14 -0400

    Comparison.java + Implemented rest of skeleton
    ComponentVersion.java
    + Base skeleton file

--- core/src/main/java/org/nvip/plugfest/tooling/differ/Comparison.java
+++ core/src/main/java/org/nvip/plugfest/tooling/differ/Comparison.java
@@ -1,4 +1,37 @@
 package org.nvip.plugfest.tooling.differ;
 
+import org.nvip.plugfest.tooling.sbom.SBOM;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Comparison {
+
+    // Target SBOM (Ground Truth) for comparison.
+    private SBOM targetSBOM;
+
+    // List of diff reports
+    private List<DiffReport> diffReportList;
+
+    // Set of comparisons
+    private Map<String, Set<ComponentVersion>> comparisons;
+
+    public Comparison() {
+
+    }
+
+    public SBOM getTargetSBOM() {
+        return this.targetSBOM;
+    }
+
+    public List<DiffReport> getDiffReports() {
+        return this.getDiffReports();
+    }
+
+    public Map<String, Set<ComponentVersion>> getComparisons() {
+        return this.comparisons;
+    }
+
+
 }
