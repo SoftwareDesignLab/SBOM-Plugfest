2ca0ecd018850c277c7e701cf9aa035e7b378210
Juan Patino <juan9patino9@gmail.com>
2023-04-12 13:35:00 -0400

    UniqueIdOccurrence implemented

--- core/src/main/java/org/nvip/plugfest/tooling/differ/UniqueIdOccurrence.java
+++ core/src/main/java/org/nvip/plugfest/tooling/differ/UniqueIdOccurrence.java
@@ -1,7 +1,9 @@
 package org.nvip.plugfest.tooling.differ;
 
 import org.apache.commons.compress.harmony.pack200.NewAttributeBands;
+import org.nvip.plugfest.tooling.sbom.SBOM;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class UniqueIdOccurrence {
@@ -15,16 +17,39 @@ public class UniqueIdOccurrence {
     // Type of Unique ID
     private UniqueIdentifierType uniqueIdType;
 
-    public UniqueIdOccurrence () {
-
+    public UniqueIdOccurrence (String uID, UniqueIdentifierType type) {
+        this.uniqueIdentifier = uID;
+        this.uniqueIdType = type;
     }
+
+    // getters
 
     public String getUniqueId() {
         return this.uniqueIdentifier;
+    }
+
+    public UniqueIdentifierType getUniqueIdType() {
+        return uniqueIdType;
     }
 
     public Set<Integer> getAppearances() {
         return this.appearances;
     }
 
+    // add individual SBOM to appearances
+    public void addAppearance(int s){
+        appearances.add(s);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UniqueIdOccurrence that)) return false;
+        return Objects.equals(uniqueIdentifier, that.uniqueIdentifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uniqueIdentifier);
+    }
 }
