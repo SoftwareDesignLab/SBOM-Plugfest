859ff5175e67ee54a47d0fc9f205395b71d4b2f0
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-04-12 18:30:59 -0400

    Added button component

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { IconComponent } from './shared/components/icon/icon.component';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IconComponent
+    IconComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ gui/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['../components.css'],
+})
+export class ButtonComponent {
+  @Input() text: string = "";
+  @Input() icon: string = "";
+}
