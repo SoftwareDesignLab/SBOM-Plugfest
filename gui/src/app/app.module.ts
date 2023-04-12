0a2c360a6f4feebaa08927d69fab1469b2b31801
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-04-12 18:14:50 -0400

    Added icon component

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconComponent } from './shared/components/icon/icon.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
