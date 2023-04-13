491023eb085bbafa58dfdd3cf1f0e0ce6eb50a3f
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-04-12 20:51:16 -0400

    Added accordian component (style not override)

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconComponent } from './shared/components/icon/icon.component';
 import { ButtonComponent } from './shared/components/button/button.component';
+import { AccordianComponent } from './shared/components/accordian/accordian.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     IconComponent,
-    ButtonComponent
+    ButtonComponent,
+    AccordianComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ gui/src/app/shared/components/accordian/accordian.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AccordianComponent } from './accordian.component';
+
+describe('AccordianComponent', () => {
+  let component: AccordianComponent;
+  let fixture: ComponentFixture<AccordianComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AccordianComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AccordianComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
