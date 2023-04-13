c731b8c00e83a328e08495a6aab773383baf5a45
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-04-13 19:21:48 -0400

    Accordian skeleton done, just need header and styling

--- gui/src/app/features/comparison/comparison/comparison.component.ts
+++ gui/src/app/features/comparison/comparison/comparison.component.ts
@@ -6,20 +6,10 @@ import {mockupDiffReport, DiffReport} from '../diffreport';
   templateUrl: './comparison.component.html',
   styleUrls: ['./comparison.component.css']
 })
-export class ComparisonComponent implements OnInit {
 
+export class ComparisonComponent {
   //@Input() diffReport: DiffReport;
   diffReport: DiffReport = mockupDiffReport;
-  path: string[] = ['components'];
-  step: string = 'components';
-
-  // I was just going to add each step thru the sbom to the path to navigate.
-  // step would be the actual view atm
-  // when u go back u just pop/slice items out of the array 
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  path: number[] = [];
+  depth: number = 0;
 }
