470b0d3afebb3191da46cb6c64d3ed049fd9bf8b
txdvse <txdvse@rit.edu>
2023-04-13 10:52:03 -0400

    ComparisonTest.java + Initial test skeleton

--- core/src/test/java/org/nvip/plugfest/tooling/differ/ComparisonTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/differ/ComparisonTest.java
@@ -1,4 +1,53 @@
 package org.nvip.plugfest.tooling.differ;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class ComparisonTest {
+
+    /**
+     * Test Constants
+     */
+
+    public static final String TEST_SMALL_CDX_JSON = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/sbom.alpine.json";
+
+    public static final String TEST_MEDIUM_CDX_JSON ="src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/trivy-0.39.0_celery-3.1.cdx.json";
+
+    public static final String TEST_ANOTHER_SMALL_SYFT_CDX_JSON = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/cdx.json";
+
+    /**
+     * Setup Teardown Methods
+     */
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @AfterEach
+    public void teardown() {
+
+    }
+
+
+    /**
+     * runComparison tests
+     */
+
+
+    /**
+     * assignComponents tests
+     */
+
+    /**
+     * getTargetSBOM tests
+     */
+
+    /**
+     * getDiffReports
+     */
+
+    /**
+     * getComparisons
+     */
+
 }
