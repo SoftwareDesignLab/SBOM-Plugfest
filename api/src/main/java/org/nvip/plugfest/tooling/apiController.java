678fe0d30671f9f81bed13deffc5bb739beb1db1
Asa Horn <horntx3@gmail.com>
2023-04-13 16:33:13 -0400

    implement compare and qa endpoints

--- api/src/main/java/org/nvip/plugfest/tooling/apiController.java
+++ api/src/main/java/org/nvip/plugfest/tooling/apiController.java
@@ -1,9 +1,88 @@
 package org.nvip.plugfest.tooling;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.nvip.plugfest.tooling.qa.QAPipeline;
+import org.nvip.plugfest.tooling.qa.QualityReport;
+import org.nvip.plugfest.tooling.sbom.SBOM;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 public class apiController {
-    //TODO: implement SBOM info request
 
-    //TODO: implement comparison request
+    private static ObjectMapper mapper;
 
-    //TODO: implement QA request
+    private static QAPipeline pipeline;
+
+    public apiController() {
+        mapper = new ObjectMapper();
+        pipeline = new QAPipeline();
+    }
+
+    @RequestMapping(value="compare", method=RequestMethod.POST)
+    public ResponseEntity<String> compare(@RequestBody String encoded) {
+        ArrayList<SBOM> sboms;
+        try {
+            sboms = decode(encoded);
+        } catch (JsonMappingException e) {
+            return new ResponseEntity<>("Body was not valid SBOM object\n received body was " + encoded, HttpStatus.BAD_REQUEST);
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("Body was not valid JSON\n received body was " + encoded, HttpStatus.BAD_REQUEST);
+        }
+
+        //todo: uncomment when comparer changes are merged.
+//        DiffReport report = Comparer.generateReport(sboms.get(0), sboms.subList(1, sboms.size()));
+//        try {
+//            return new ResponseEntity<>(encode(report), HttpStatus.OK);
+//        } catch (JsonProcessingException e) {
+//            return new ResponseEntity<>("Error processing report", HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+
+        return new ResponseEntity<>("Not implemented", HttpStatus.NOT_IMPLEMENTED); //also remove this!!
+    }
+
+    @RequestMapping(value="qa", method=RequestMethod.POST)
+    public ResponseEntity<String> qa(@RequestBody String encoded) {
+        ArrayList<SBOM> sboms;
+        //decode post body
+        try {
+            sboms = decode(encoded);
+        } catch (JsonMappingException e) {
+            return new ResponseEntity<>("Body was not valid SBOM object\n received body was " + encoded, HttpStatus.BAD_REQUEST);
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("Body was not valid JSON\n received body was " + encoded, HttpStatus.BAD_REQUEST);
+        }
+
+        //some basic validation
+        if(sboms.size() != 1){
+            return new ResponseEntity<>("QA request must contain only one SBOM", HttpStatus.BAD_REQUEST);
+        }
+
+        //run the QA
+        QualityReport report = pipeline.process(sboms.get(0));
+
+        //encode and send report
+        try {
+            return new ResponseEntity<>(encode(report), HttpStatus.OK);
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("Error processing report", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private ArrayList<SBOM> decode(String encoded) throws JsonProcessingException, JsonMappingException {
+        ArrayList<SBOM> sboms = new ArrayList<>();
+        for(String line : encoded.split("\n")){
+            sboms.add(mapper.readValue(line, SBOM.class));
+        }
+
+        return sboms;
+    }
+
+    private String encode(Object o) throws JsonProcessingException {
+        return mapper.writeValueAsString(o);
+    }
+
 }
