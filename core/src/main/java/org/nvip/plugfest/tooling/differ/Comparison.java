d77a479edc911bb7ba3529251392b70815ec77b4
txdvse <txdvse@rit.edu>
2023-04-13 10:37:12 -0400

    Comparison.java + Partial implementation of assignComponents function

--- core/src/main/java/org/nvip/plugfest/tooling/differ/Comparison.java
+++ core/src/main/java/org/nvip/plugfest/tooling/differ/Comparison.java
@@ -1,10 +1,10 @@
 package org.nvip.plugfest.tooling.differ;
 
+import org.cyclonedx.model.Diff;
+import org.nvip.plugfest.tooling.sbom.Component;
 import org.nvip.plugfest.tooling.sbom.SBOM;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Comparison {
 
@@ -17,8 +17,37 @@ public class Comparison {
     // Set of comparisons
     private Map<String, Set<ComponentVersion>> comparisons;
 
-    public Comparison() {
+    public Comparison(SBOM target, List<SBOM> stream) {
+        this.targetSBOM = target;
+        this.diffReportList = new ArrayList<>();
+        this.comparisons = new HashMap<>();
+    }
 
+    public List<DiffReport> runComparison(List<SBOM> stream) {
+        for(SBOM current_sbom : stream) {
+            assignComponents(current_sbom);
+            diffReportList.add(Comparer.generateReport(targetSBOM, current_sbom));
+        }
+    }
+
+    private void assignComponents(SBOM current_sbom) {
+
+        // Loop through all components in SBOM and add them to comparisons list.
+        for(Component current_component : current_sbom.getAllComponents()) {
+
+            // Create a temporary ComponentVersion object for the current SBOM component
+            ComponentVersion temporary_cv = new ComponentVersion(current_component.getName(), current_component.getVersion());
+
+            // If the comparisons collection contains
+            if(comparisons.containsKey(current_component.getName())) {
+
+                // Get that component's component version collection
+                Set<ComponentVersion> current_cv_list = comparisons.get(current_component.getName());
+
+            } else {
+
+            }
+        }
     }
 
     public SBOM getTargetSBOM() {
