5567a91f53622ab7f2f2e6a99e636e9c2a7a66c7
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-04-12 20:51:16 -0400

    Added accordian component (style not override)

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { IconComponent } from './shared/components/icon/icon.component';
 import { ButtonComponent } from './shared/components/button/button.component';
+import { AccordianComponent } from './shared/components/accordian/accordian.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     IconComponent,
-    ButtonComponent
+    ButtonComponent,
+    AccordianComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ gui/src/app/shared/components/accordian/accordian.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-accordian',
+  templateUrl: './accordian.component.html',
+  styleUrls: ['../components.css']
+})
+export class AccordianComponent {
+  @Input() title: string = '';
+  @Input() extra: string[] = [];
+  @Input() color: string = '';
+  @Input() content: string = '';
+}
