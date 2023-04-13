235399c1d7580cb219402836e70dfc45c6e83f96
Asa Horn <horntx3@gmail.com>
2023-04-13 12:22:36 -0400

    name changes

--- api/src/main/java/org/nvip/plugfest/tooling/apiController.java
+++ api/src/main/java/org/nvip/plugfest/tooling/apiController.java
@@ -1,11 +1,9 @@
 package org.nvip.plugfest.tooling;
 
 public class apiController {
-    //TODO: implement SBOM translate request
-
-    //TODO: implement SBOM data request
+    //TODO: implement SBOM info request
 
     //TODO: implement comparison request
 
-    //TODO: implement metrics request
+    //TODO: implement QA request
 }
