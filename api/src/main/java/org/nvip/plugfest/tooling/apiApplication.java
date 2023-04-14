4cbf00796a8a7dbf245ebdaf5252cbff3099005b
Asa Horn <horntx3@gmail.com>
2023-04-14 19:07:58 -0400

    *fixed* explicitly set WebApplicationType

--- api/src/main/java/org/nvip/plugfest/tooling/apiApplication.java
+++ api/src/main/java/org/nvip/plugfest/tooling/apiApplication.java
@@ -1,11 +1,15 @@
 package org.nvip.plugfest.tooling;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class apiApplication{
     public static void main(String[] args) {
-        SpringApplication.run(apiController.class, args);
+        new SpringApplicationBuilder(apiApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
     }
 }
