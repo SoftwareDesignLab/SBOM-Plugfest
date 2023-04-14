d4e9854f9837dc5f8cdc8f41f979c3170a619dae
“tina-dilorenzo” <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-04-14 09:39:59 -0400

    Fixes app.module and content projection for the comparitor

---
+++ gui/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatMenuModule } from '@angular/material/menu';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { IconComponent } from './shared/components/icon/icon.component';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { AccordionComponent } from './shared/components/accordion/accordion.component';
+import { DropdownComponent } from '@components/dropdown/dropdown.component';
+import { SidePanelComponent } from './features/comparison/side-panel/side-panel.component';
+import { ComparisonComponent } from './features/comparison/comparison/comparison.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    IconComponent,
+    ButtonComponent,
+    AccordionComponent,
+    IconComponent,
+    DropdownComponent,
+    SidePanelComponent,
+    ComparisonComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatMenuModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatExpansionModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
