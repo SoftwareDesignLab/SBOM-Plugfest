728f8267454d5741235e86ae392f5783aadeda8d
“tina-dilorenzo” <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-04-17 09:40:05 -0400

    Move side panel location

--- gui/src/app/features/comparison/comparison-page/comparison-page.component.ts
+++ gui/src/app/features/comparison/comparison-page/comparison-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { Comparison, comparisonMockup } from '../comparison';
+import { SBOM } from '@models/sbom';
 
 @Component({
   selector: 'app-comparison-page',
@@ -7,11 +8,40 @@ import { Comparison, comparisonMockup } from '../comparison';
   styleUrls: ['./comparison-page.component.css'],
 })
 export class ComparisonPageComponent {
-  comparison: Comparison | null = null;
-  // @TODO: apply api calls here with SBOM A & SBOM B
-  constructor() {}
+  collapsed: boolean = false;
+
+  sboms: SBOM[] = [];
+  selectionSBOMS: SBOM[] = [];  // to display in options
+  targetSbom: SBOM | null = null; // number is for original position
+  comparisonSBOM: SBOM | null = null;
+  comparison:Comparison| null = null;
+
+  uploadSbom($event: any) {
+    // @TODO: FILE UPLOAD HERE
+    // apiCall with event.target.files
+    this.sboms = [{ name: 'SBOM A' }, { name: 'SBOM B' }, { name: 'SBOM C' }];
+    this.selectionSBOMS = this.sboms;
+  }
+
+
+  selectTargetSbom($event: any) {
+    this.targetSbom = $event;
+    this.selectionSBOMS = this.sboms.filter(
+      (s) => s.name !== this.targetSbom?.name
+    );
+    this.compare();
+  }
+
+  selectComparison($event: any){
+    this.comparisonSBOM = $event.target?.value || null
+    this.compare();
+  }
 
   compare() {
-    this.comparison = comparisonMockup;
+    if (this.targetSbom && this.comparisonSBOM) {
+      //this.comparison = [this.targetSbom, this.comparisonSBOM];
+      this.comparison = comparisonMockup;
+    }
   }
 }
+
---
+++ gui/src/app/shared/components/side-panel/side-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-side-panel',
+  templateUrl: './side-panel.component.html',
+  styleUrls: ['./side-panel.component.css'],
+})
+export class SidePanelComponent {
+  collapsed: boolean = false;
+  @Input() tabText: string = '';
+}
