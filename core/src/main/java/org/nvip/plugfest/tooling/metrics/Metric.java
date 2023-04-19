8781e0356a25599b9e1e71b49974bcc6e7dad1a2
Asa Horn <horntx3@gmail.com>
2023-04-19 18:09:07 -0400

    Docstring, comment, organization pass.

--- core/src/main/java/org/nvip/plugfest/tooling/metrics/Metric.java
+++ core/src/main/java/org/nvip/plugfest/tooling/metrics/Metric.java
@@ -3,10 +3,27 @@ package org.nvip.plugfest.tooling.metrics;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Abstract class for metrics
+ *
+ * @author (names)
+ */
 public abstract class Metric {
+    /**
+     * The score the SBOM received for this metric.
+     */
     protected int score;
+
+    /**
+     * Implementations should override this function to run the actual tests. Consider calling smaller tests from this
+     * main function.
+     *
+     * @return - Integer score this SBOM received for this metric.
+     */
+    protected abstract int testMetric();
+
+    //getter
     public int getScore() {
         return this.score;
     }
-    protected abstract int testMetric();
 }
