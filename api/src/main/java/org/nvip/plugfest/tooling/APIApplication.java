59d383bf30e83d94686bef9daffadd04b34c402c
Asa Horn <horntx3@gmail.com>
2023-04-19 14:49:55 -0400

    Doc creation / update

--- api/src/main/java/org/nvip/plugfest/tooling/APIApplication.java
+++ api/src/main/java/org/nvip/plugfest/tooling/APIApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+/**
+ * This class contains the main function which runs the API as a spring boot application
+ */
 @SpringBootApplication
 public class APIApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(APIApplication.class)
-                .web(WebApplicationType.SERVLET)
+                .web(WebApplicationType.SERVLET) //This is required to prevent the dreaded "No valid webserver" error. Ask me how I know....
                 .run(args);
     }
 }
