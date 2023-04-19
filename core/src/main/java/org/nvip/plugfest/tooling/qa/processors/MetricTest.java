1d6cbb0fa7cf57e30e5352220851106e98a86b55
Asa Horn <horntx3@gmail.com>
2023-04-19 18:09:10 -0400

    Docstring, comment, organization pass.

--- core/src/main/java/org/nvip/plugfest/tooling/qa/processors/MetricTest.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/processors/MetricTest.java
@@ -5,6 +5,8 @@ import org.nvip.plugfest.tooling.sbom.Component;
 
 /**
  * Abstract class to be extended by all metric tests
+ *
+ * @author Dylan Mulligan
  */
 public abstract class MetricTest {
     //#region Attributes
