176949da489ca0b50abfb9f27f0af413b18e2ee9
txdvse <txdvse@rit.edu>
2023-04-19 19:42:47 -0400

    CDXMetricsTest.java + Added fake sbom file path + Added a test for getFilepath + Added a test for setFilepath

--- core/src/test/java/org/nvip/plugfest/tooling/metrics/CDXMetricsTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/metrics/CDXMetricsTest.java
@@ -2,6 +2,7 @@ package org.nvip.plugfest.tooling.metrics;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CDXMetricsTest {
@@ -11,6 +12,8 @@ public class CDXMetricsTest {
      */
 
     private static final String TEST_CDX_JSON_SBOM_PATH = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/";
+
+    private static final String TEST_FAKE_CDX_SBOM_PATH = "src/test/java/org/nvip/plugfest/tooling/fake_sbom_path/";
 
     private static final String TEST_CDX_SBOM_ONE = "trivy-0.39.0_celery-3.1.cdx.json";
 
@@ -35,6 +38,26 @@ public class CDXMetricsTest {
 
         CDXMetrics test_cdx_metric = new CDXMetrics(TEST_CDX_JSON_SBOM_PATH, TEST_CDX_SBOM_ONE);
         assertNotNull(test_cdx_metric);
+
+    }
+
+    @Test
+    public void CDXMetrics_getFilepath_test() {
+
+        CDXMetrics test_cdx_metric = new CDXMetrics(TEST_CDX_JSON_SBOM_PATH, TEST_CDX_SBOM_ONE);
+        assertNotNull(test_cdx_metric);
+        String file_path_result = test_cdx_metric.getFilepath();
+        assertEquals(TEST_CDX_JSON_SBOM_PATH, file_path_result);
+
+    }
+
+    @Test
+    public void CDXMetrics_setFilepath_test() {
+
+        CDXMetrics test_cdx_metric = new CDXMetrics(TEST_CDX_JSON_SBOM_PATH, TEST_CDX_SBOM_ONE);
+        assertNotNull(test_cdx_metric);
+        test_cdx_metric.setFilepath(TEST_FAKE_CDX_SBOM_PATH);
+        assertEquals(TEST_FAKE_CDX_SBOM_PATH, test_cdx_metric.getFilepath());
 
     }
 
