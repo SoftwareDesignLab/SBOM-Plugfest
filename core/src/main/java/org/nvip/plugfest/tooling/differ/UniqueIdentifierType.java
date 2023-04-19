0d406e353c49ad3162a84baac3799cff8b00ba79
Asa Horn <horntx3@gmail.com>
2023-04-19 18:09:56 -0400

    Docstring, comment, organization pass.

--- core/src/main/java/org/nvip/plugfest/tooling/differ/UniqueIdentifierType.java
+++ core/src/main/java/org/nvip/plugfest/tooling/differ/UniqueIdentifierType.java
@@ -1,5 +1,10 @@
 package org.nvip.plugfest.tooling.differ;
 
+/**
+ * Enum to represent the different types of unique identifiers
+ *
+ * @author Txdvse (who is this?)
+ */
 public enum UniqueIdentifierType {
     CPE,
     PURL,
