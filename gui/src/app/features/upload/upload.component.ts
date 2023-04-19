3d7e0576de35461cf9de3f2ff02afcc5d57809f2
Matt London <mrl2534@rit.edu>
2023-04-19 15:50:09 -0400

    Get a list of selected files when the browse function is invoked

--- gui/src/app/features/upload/upload.component.ts
+++ gui/src/app/features/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IpcRenderer } from 'electron';
 
 @Component({
   selector: 'app-upload',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  files: [] = [];
+  private ipc!: IpcRenderer;
+
+  constructor() {
+    if (window.require) {
+      try {
+        this.ipc = window.require('electron').ipcRenderer;
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.warn('App not running inside Electron!');
+    }
+  }
+
+  browse() {
+    this.ipc.invoke('selectFiles').then((files) => {
+      if(files === undefined || files === "" || files === null) {
+        this.files = [];
+        return;
+      }
+
+      this.files = files;
+    });
+  }
+
 
 }
