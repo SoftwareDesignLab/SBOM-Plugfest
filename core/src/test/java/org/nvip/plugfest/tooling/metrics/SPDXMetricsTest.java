d1137af69bb3b5772d70410bbd582838195e17f8
txdvse <txdvse@rit.edu>
2023-04-20 18:29:29 -0400

    SPDXMetricsTest.java + Added setFilepath test

--- core/src/test/java/org/nvip/plugfest/tooling/metrics/SPDXMetricsTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/metrics/SPDXMetricsTest.java
@@ -12,6 +12,8 @@ public class SPDXMetricsTest {
      */
     private static final String TEST_SPDX_SBOM_PATH = "src/test/java/org/nvip/plugfest/tooling/sample_boms";
 
+    private static final String TEST_FAKE_SPDX_SBOM_PATH = "src/test/java/org/nvip/plugfest/tooling/fake_boms";
+
     private static final String TEST_SPDX_SBOM_ONE = "sbom.docker.2-2.spdx";
 
     /**
@@ -38,6 +40,15 @@ public class SPDXMetricsTest {
         assertNotNull(test_spdx_metric);
         String file_path_result = test_spdx_metric.getFilepath();
         assertEquals(TEST_SPDX_SBOM_PATH, file_path_result);
+    }
+
+    @Test
+    public void SPDXMetrics_setFilepath_test() {
+        SPDXMetrics test_spdx_metric = new SPDXMetrics(TEST_SPDX_SBOM_PATH, TEST_SPDX_SBOM_ONE);
+        assertNotNull(test_spdx_metric);
+        test_spdx_metric.setFilepath(TEST_FAKE_SPDX_SBOM_PATH);
+        String file_path_result = test_spdx_metric.getFilepath();
+        assertEquals(TEST_FAKE_SPDX_SBOM_PATH, file_path_result);
     }
 
 }
