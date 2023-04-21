5ed31636759f991c47e2c9d4852b1e7a4d8d02a9
txdvse <txdvse@rit.edu>
2023-04-20 23:17:05 -0400

    TestTest.java + Added basic test and skeleton

--- core/src/test/java/org/nvip/plugfest/tooling/qa/test_results/TestTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/qa/test_results/TestTest.java
@@ -1,4 +1,22 @@
 package org.nvip.plugfest.tooling.qa.test_results;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TestTest {
+
+    /**
+     * Test Constants
+     */
+    private static final String test_pass_message = "This test has passed.";
+
+    /**
+     * Test Constructor Test
+     */
+
+    @org.junit.jupiter.api.Test
+    public void create_Test_test() {
+        Test test_test = new Test(true, test_pass_message);
+        assertNotNull(test_test);
+    }
+
 }
