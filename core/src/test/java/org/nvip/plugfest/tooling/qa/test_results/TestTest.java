b0b75c7fa3cff6cad970333b5ff87eb20074d13d
txdvse <txdvse@rit.edu>
2023-04-20 23:34:52 -0400

    TestTest.java + Added two toString tests

--- core/src/test/java/org/nvip/plugfest/tooling/qa/test_results/TestTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/qa/test_results/TestTest.java
@@ -7,7 +7,11 @@ public class TestTest {
     /**
      * Test Constants
      */
-    private static final String TEST_PASS_MESSAGE = "This test has passed.";
+    private static final String TEST_MESSAGE = "This is a test.";
+
+    private static final String TO_STRING_PASS_MESSAGE = "PASSED: " + TEST_MESSAGE;
+
+    private static final String TO_STRING_FAIL_MESSAGE = "FAILED: " + TEST_MESSAGE;
 
     /**
      * Test Constructor Test
@@ -15,22 +19,38 @@ public class TestTest {
 
     @org.junit.jupiter.api.Test
     public void create_Test_test() {
-        Test test_test = new Test(true, TEST_PASS_MESSAGE);
+        Test test_test = new Test(true, TEST_MESSAGE);
         assertNotNull(test_test);
     }
 
     @org.junit.jupiter.api.Test
     public void Test_getStatus_test() {
-        Test test_test = new Test(true, TEST_PASS_MESSAGE);
+        Test test_test = new Test(true, TEST_MESSAGE);
         assertNotNull(test_test);
         assertTrue(test_test.getStatus());
     }
 
     @org.junit.jupiter.api.Test
     public void Test_getMessage_test() {
-        Test test_test = new Test(true, TEST_PASS_MESSAGE);
+        Test test_test = new Test(true, TEST_MESSAGE);
         assertNotNull(test_test);
-        assertEquals(TEST_PASS_MESSAGE, test_test.getMessage());
+        assertEquals(TEST_MESSAGE, test_test.getMessage());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void Test_toString_pass_message_test() {
+        Test test_test = new Test(true, TEST_MESSAGE);
+        assertNotNull(test_test);
+        String toString_result = test_test.toString();
+        assertEquals(TO_STRING_PASS_MESSAGE, toString_result);
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void Test_toString_fail_message_test() {
+        Test test_test = new Test(false, TEST_MESSAGE);
+        assertNotNull(test_test);
+        String toString_result = test_test.toString();
+        assertEquals(TO_STRING_FAIL_MESSAGE, toString_result);
     }
 
 }
