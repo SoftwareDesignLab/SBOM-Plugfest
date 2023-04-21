14235e5637ff618cbf7a7adfd5c2bd7e741371f8
txdvse <txdvse@rit.edu>
2023-04-21 16:50:14 -0400

    TestResultsTest.java + Added addTests tests

--- core/src/test/java/org/nvip/plugfest/tooling/qa/test_results/TestResultsTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/qa/test_results/TestResultsTest.java
@@ -2,12 +2,12 @@ package org.nvip.plugfest.tooling.qa.test_results;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.nvip.plugfest.tooling.sbom.*;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,9 +21,16 @@ public class TestResultsTest {
 
     private static final String TEST_MESSAGE_TWO = "This is also a test.";
 
+    private static final String TEST_MESSAGE_THREE = "This may as well be test.";
+
+    private static final String TEST_MESSAGE_FOUR = "This is indeed again, a test.";
+
+
     private static final String TO_STRING_PASS_MESSAGE = "PASSED: " + TEST_MESSAGE;
 
     private static final String TO_STRING_FAIL_MESSAGE = "FAILED: " + TEST_MESSAGE;
+
+    private static final int EXPECTED_ADDTESTS_RESULT = 3;
 
     /**
      * Test Variables
@@ -34,6 +41,13 @@ public class TestResultsTest {
     Component test_component_b;
 
     org.nvip.plugfest.tooling.qa.test_results.Test test_test_a;
+
+    org.nvip.plugfest.tooling.qa.test_results.Test test_test_b;
+
+    org.nvip.plugfest.tooling.qa.test_results.Test test_test_c;
+
+    org.nvip.plugfest.tooling.qa.test_results.Test test_test_d;
+
 
     /**
      * Setup
@@ -53,6 +67,13 @@ public class TestResultsTest {
         );
 
         test_test_a = new org.nvip.plugfest.tooling.qa.test_results.Test(true, TEST_MESSAGE);
+
+        test_test_b = new org.nvip.plugfest.tooling.qa.test_results.Test(true, TEST_MESSAGE_TWO);
+
+        test_test_c = new org.nvip.plugfest.tooling.qa.test_results.Test(true, TEST_MESSAGE_THREE);
+
+        test_test_d = new org.nvip.plugfest.tooling.qa.test_results.Test(true, TEST_MESSAGE_FOUR);
+
 
     }
 
@@ -114,17 +135,122 @@ public class TestResultsTest {
     }
 
     @Test
-    public void TestResult_addTest_to_a_null_component() {
+    public void TestResult_addTest_to_a_null_component_test() {
         TestResults test_tr = new TestResults(test_component_a);
         assertNotNull(test_tr);
         test_tr.addTest(test_test_a);
     }
 
     @Test
-    public void TestResult_addTest_should_not_work_on_null_TestResults() {
+    public void TestResult_addTest_should_not_work_on_null_TestResults_test() {
         TestResults test_tr = null;
         assertNull(test_tr);
         assertThrows(NullPointerException.class, () -> test_tr.addTest(test_test_a));
     }
+
+    /**
+     * getTests Tests
+     */
+    @Test
+    public void TestResult_getTest_test() {
+        TestResults test_tr = new TestResults(test_component_a);
+        assertNotNull(test_tr);
+        test_tr.addTest(test_test_a);
+        List<org.nvip.plugfest.tooling.qa.test_results.Test> getTestResults = test_tr.getTests();
+        assertNotNull(getTestResults);
+    }
+
+    @Test
+    public void TestResult_getTest_should_have_correct_results_test() {
+        TestResults test_tr = new TestResults(test_component_a);
+        assertNotNull(test_tr);
+        test_tr.addTest(test_test_a);
+        List<org.nvip.plugfest.tooling.qa.test_results.Test> getTestResults = test_tr.getTests();
+        assertNotNull(getTestResults);
+        assertEquals(test_test_a, getTestResults.get(0));
+    }
+
+    @Test
+    public void TestResult_getTest_should_be_empty_for_TestResult_with_no_tests_test() {
+        TestResults test_tr = new TestResults(test_component_a);
+        assertNotNull(test_tr);
+        List<org.nvip.plugfest.tooling.qa.test_results.Test> getTestResults = test_tr.getTests();
+        assertTrue(getTestResults.isEmpty());
+    }
+
+    @Disabled("Possible Bug; do we want test results exists for a TestResult object without a component?")
+    @Test
+    public void TestResult_getTest_should_return_nothing_for_TestResult_with_no_component_test() {
+        TestResults test_tr = new TestResults(null);
+        assertNotNull(test_tr);
+        test_tr.addTest(test_test_a);
+        List<org.nvip.plugfest.tooling.qa.test_results.Test> getTestResults = test_tr.getTests();
+        assertNotNull(getTestResults);
+        assertEquals(test_test_a, getTestResults.get(0));
+    }
+
+    /**
+     * addTests Tests
+     */
+
+    @Test
+    public void TestResult_addTests_test() {
+        TestResults test_tr_one = new TestResults(test_component_a);
+        assertNotNull(test_tr_one);
+        test_tr_one.addTest(test_test_a);
+        test_tr_one.addTest(test_test_b);
+        test_tr_one.addTest(test_test_c);
+
+        TestResults test_tr_two = new TestResults(test_component_b);
+        assertNotNull(test_tr_two);
+        test_tr_two.addTests(test_tr_one);
+    }
+
+    @Test
+    public void TestResult_addTests_has_correct_tests_test() {
+        TestResults test_tr_one = new TestResults(test_component_a);
+        assertNotNull(test_tr_one);
+        test_tr_one.addTest(test_test_a);
+        test_tr_one.addTest(test_test_b);
+        test_tr_one.addTest(test_test_c);
+
+        TestResults test_tr_two = new TestResults(test_component_b);
+        assertNotNull(test_tr_two);
+        test_tr_two.addTests(test_tr_one);
+
+        assertEquals(EXPECTED_ADDTESTS_RESULT, test_tr_two.getTests().size());
+
+        assertEquals(test_tr_one.getTests(), test_tr_two.getTests());
+    }
+
+    @Test
+    public void TestResult_addTests_should_merge_tests_test() {
+        TestResults test_tr_one = new TestResults(test_component_a);
+        assertNotNull(test_tr_one);
+        test_tr_one.addTest(test_test_a);
+        test_tr_one.addTest(test_test_b);
+
+        TestResults test_tr_two = new TestResults(test_component_b);
+        assertNotNull(test_tr_two);
+        test_tr_two.addTest(test_test_c);
+        test_tr_two.addTest(test_test_d);
+
+        test_tr_two.addTests(test_tr_one);
+
+        ArrayList<org.nvip.plugfest.tooling.qa.test_results.Test> expected = new ArrayList<>();
+        expected.add(test_test_a);
+        expected.add(test_test_b);
+        expected.add(test_test_c);
+        expected.add(test_test_d);
+        ArrayList<org.nvip.plugfest.tooling.qa.test_results.Test> result = test_tr_two.getTests();
+
+        for(org.nvip.plugfest.tooling.qa.test_results.Test current_test : result) {
+            assertTrue(expected.contains(current_test));
+        }
+        for(org.nvip.plugfest.tooling.qa.test_results.Test current_test : expected) {
+            assertTrue(expected.contains(current_test));
+        }
+    }
+
 
 }
