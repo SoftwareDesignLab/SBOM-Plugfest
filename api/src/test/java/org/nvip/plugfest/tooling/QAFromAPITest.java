2962ccf5e6cbc60a55e2f27cdfc7e5018a0547a8
Matt London <mrl2534@rit.edu>
2023-04-25 16:34:58 -0400

    Use new inteface for the qa endpoint

--- api/src/test/java/org/nvip/plugfest/tooling/QAFromAPITest.java
+++ api/src/test/java/org/nvip/plugfest/tooling/QAFromAPITest.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -23,24 +26,40 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
  */
 public class QAFromAPITest {
 
-    private final MultipartFile smallDockerSBOM = new MockMultipartFile(System.getProperty("user.dir")
-            + "/src/test/java/org/nvip/plugfest/tooling/sample_sboms/sbom.docker.2-2_small.spdx");
-    private final MultipartFile pythonSBOM = new MockMultipartFile(System.getProperty("user.dir")
-            + "/src/test/java/org/nvip/plugfest/tooling/sample_sboms/sbom.python.2-3.spdx");
+    private final String smallDockerSBOM = System.getProperty("user.dir")
+            + "/src/test/java/org/nvip/plugfest/tooling/sample_sboms/sbom.docker.2-2_small.spdx";
+    private final String pythonSBOM = System.getProperty("user.dir")
+            + "/src/test/java/org/nvip/plugfest/tooling/sample_sboms/sbom.python.2-3.spdx";
     private APIController ctrl;
 
     @Test
     public void qaTest() {
-        ResponseEntity<QualityReport> qa = ctrl.qa(smallDockerSBOM);
-        assertEquals(qa.getStatusCode(), HttpStatus.OK);
-        assertNotEquals(qa.getBody().getPassedComponents(), 0);
+        try {
+            String contents = new String(Files.readAllBytes(Paths.get(smallDockerSBOM)));
+
+            ResponseEntity<QualityReport> qa = ctrl.qa(contents, smallDockerSBOM);
+            assertEquals(qa.getStatusCode(), HttpStatus.OK);
+            assertNotEquals(qa.getBody().getPassedComponents(), 0);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            assertEquals(1, 0);
+        }
     }
 
     @Test
     public void qaFastTest() {
-        ResponseEntity<QualityReport> qa = ctrl.qa(pythonSBOM);
-        assertEquals(qa.getStatusCode(), HttpStatus.OK);
-        assertNotEquals(qa.getBody().getPassedComponents(), 0);
+        try {
+            String contents = new String(Files.readAllBytes(Paths.get(pythonSBOM)));
+
+            ResponseEntity<QualityReport> qa = ctrl.qa(contents, pythonSBOM);
+            assertEquals(qa.getStatusCode(), HttpStatus.OK);
+            assertNotEquals(qa.getBody().getPassedComponents(), 0);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            assertEquals(1, 0);
+        }
     }
 
     @BeforeEach
