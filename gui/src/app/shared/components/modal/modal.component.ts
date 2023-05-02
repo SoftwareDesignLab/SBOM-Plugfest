27ac98f0a6ef3fd2a9b0a52065dcadc02eae645e
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-05-02 11:48:16 -0400

    modal init

---
+++ gui/src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+
+export class ModalComponent {
+  constructor(public dialogRef: MatDialogRef<ModalComponent>) {}
+}
