4b123838ddece60f512377b26b12585da85da53c
Matthew Morrison <msm8275@rit.edu>
2023-05-28 19:21:33 -0400

    Added test for pypi (From old DataVerificationTest)

--- core/src/main/java/org/nvip/plugfest/tooling/qa/tests/IsRegisteredTest.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/tests/IsRegisteredTest.java
@@ -15,12 +15,20 @@ import java.util.Set;
 public class IsRegisteredTest extends MetricTest{
 
     private static final String TEST_NAME = "IsRegistered";
+
+    /**
+     * Run the test for all components in the SBOM
+     * @param sbom the SBOM to test
+     * @return a collection of results given from the tests
+     */
     @Override
     public List<Result> test(SBOM sbom) {
         List<Result> results = new ArrayList<>();
+        // for every component, test for purls and if they are valid
         for(Component c : sbom.getAllComponents()){
             results.addAll(testComponentPURLs(c));
         }
+        // return findings of tests
         return results;
     }
 
@@ -32,21 +40,47 @@ public class IsRegisteredTest extends MetricTest{
         Set<PURL> purls = c.getPurls();
         // if no purls are present, the test automatically fails
         if(purls.isEmpty()){
-            r = new Result(TEST_NAME, Result.STATUS.FAIL, "Component has no PURL");
+            r = new Result(TEST_NAME, Result.STATUS.FAIL,
+                    "Component has no PURL");
             r.addContext(c, "PURL Validation");
             purlResults.add(r);
             return purlResults;
         }
         else{
-            // check the purl based on its type
+            // check all purl based on its type
             for(PURL p : c.getPurls()){
+                // holds the response code from the purl
                 int response;
                 try{
                     switch(p.getType().toLowerCase()) {
                         case "maven" -> response =  extractFromMaven(p);
+                        case "pypi" -> response = extractFromPyPi(p);
+                        // an invalid PURL type
+                        default -> {
+                            r = new Result(TEST_NAME, Result.STATUS.FAIL,
+                                    "PURL is not a valid type");
+                            r.addContext(c, "PURL Validation");
+                            purlResults.add(r);
+                        }
                     }
                 }catch(IOException e){
-
+                    r = new Result(TEST_NAME, Result.STATUS.FAIL,
+                            "PURL had an error");
+                    r.addContext(c, "PURL Validation");
+                    purlResults.add(r);
+                }
+                // if the response code is 200 (HTTP_OK), then
+                // package manager is valid
+                if(response == 200){
+                    r = new Result(TEST_NAME, Result.STATUS.PASS,
+                            "Package Manager is valid");
+                    r.addContext(c, "PURL Validation");
+                    purlResults.add(r);
+                } else{
+                    r = new Result(TEST_NAME, Result.STATUS.FAIL,
+                            "Package Manager is invalid");
+                    r.addContext(c, "PURL Validation");
+                    purlResults.add(r);
                 }
 
             }
@@ -54,6 +88,13 @@ public class IsRegisteredTest extends MetricTest{
         }
     }
 
+    /**
+     * Extract data for maven based packages.
+     * Source: <a href="https://mvnrepository.com/">...</a>
+     * @param purl purl to use to query for info
+     * @return an int response code when opening up a connection with the PURL
+     * @throws IOException issue with http connection
+     */
     public int extractFromMaven(PURL p) throws IOException{
         // maven requires namespace
         if(p.getNamespace() == null || p.getNamespace().size() == 0)
@@ -72,6 +113,24 @@ public class IsRegisteredTest extends MetricTest{
                 namespaceUrl +
                 p.getName().toLowerCase() +
                 "/" + p.getVersion());
+        HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+        huc.setRequestMethod("GET");
+        // get the response code from this url
+        return huc.getResponseCode();
+    }
+
+    /**
+     * Extract data for python based packages.
+     * Source: <a href="https://pypi.org/project/">...</a>
+     * @param purl purl to use to query for info
+     * @return an int response code when opening up a connection with the PURL
+     * @throws IOException issue with http connection
+     */
+    public int extractFromPyPi(PURL p) throws IOException {
+        // Query page
+        URL url = new URL ("https://pypi.org/project/" +
+                purl.getName().toLowerCase() +
+                (purl.getVersion() != null ? "/" + purl.getVersion() : ""));
         HttpURLConnection huc = (HttpURLConnection) url.openConnection();
         huc.setRequestMethod("GET");
         // get the response code from this url
