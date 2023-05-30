da8631c7512e2d2e7c7ddad6b316554886b307c8
dlg1206 <dlg1206@g.rit.edu>
2023-05-30 10:43:18 -0400

    add utility conflict organizer json object

--- core/src/main/java/org/nvip/plugfest/tooling/differ/DiffReport.java
+++ core/src/main/java/org/nvip/plugfest/tooling/differ/DiffReport.java
@@ -1,10 +1,14 @@
 package org.nvip.plugfest.tooling.differ;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.cyclonedx.model.Hash;
 import org.nvip.plugfest.tooling.sbom.ComponentConflict;
 import org.nvip.plugfest.tooling.sbom.SBOM;
 import org.nvip.plugfest.tooling.sbom.SBOMConflict;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -18,7 +22,7 @@ public class DiffReport {
     /**
      * Utility class for storing conflict data
      */
-    private static class Conflict{
+    private static class ConflictData {
         @JsonProperty
         private final String type;
         @JsonProperty
@@ -33,14 +37,56 @@ public class DiffReport {
          * @param target data stored in the target SBOM
          * @param current data stored in the current SBOM
          */
-        public Conflict(String type, String target, String current){
+        public ConflictData(String type, String target, String current){
             this.type = type;
             this.target = target;
             this.current = current;
         }
     }
+
+    /**
+     * Utility class for organizing conflict data
+     */
+    private static class ConflictOrganizer {
+        @JsonProperty
+        private final List<ConflictData> sbomConflicts = new ArrayList<>();
+        @JsonProperty
+        private final HashMap<String, HashMap<String, List<ConflictData>>> componentConflicts = new HashMap<>();
+
+        /**
+         * Add new conflict data for the SBOMs
+         *
+         * @param data conflict data
+         */
+        public void addSBOMConflict(ConflictData data){
+            this.sbomConflicts.add(data);
+        }
+
+
+        /**
+         * Add new conflict data for the components
+         *
+         * @param targetCUID target component uid
+         * @param otherCUID other component uid
+         * @param data conflict data
+         */
+        public void addComponentConflict(String targetCUID, String otherCUID, ConflictData data){
+            // add new target component if it doesn't exist
+            this.componentConflicts.computeIfAbsent(targetCUID, k -> new HashMap<>());
+
+            // add new other component if it doesn't exist
+            this.componentConflicts.get(targetCUID).computeIfAbsent(otherCUID, k -> new ArrayList<>());
+
+            // add the conflict data
+            this.componentConflicts.get(targetCUID).get(otherCUID).add(data);
+        }
+    }
+
+
     @JsonProperty("target")
     private String targetUID;
+
+    private HashMap<String, String>
 
     private SBOM targetSBOM;
     /** Stores conflicts in non-trivial information within these sboms */
