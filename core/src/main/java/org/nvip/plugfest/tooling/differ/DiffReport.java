b7b8dfa7a93a7c4335dc44c6186d4e188437d348
dlg1206 <dlg1206@g.rit.edu>
2023-05-30 16:36:17 -0400

    initial json object

--- core/src/main/java/org/nvip/plugfest/tooling/differ/DiffReport.java
+++ core/src/main/java/org/nvip/plugfest/tooling/differ/DiffReport.java
@@ -1,6 +1,7 @@
 package org.nvip.plugfest.tooling.differ;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.nvip.plugfest.tooling.sbom.*;
 
 import java.util.*;
@@ -13,6 +14,7 @@ import static org.nvip.plugfest.tooling.sbom.ComponentConflictType.*;
  * @author Matt London
  * @author Derek Garcia
  */
+@JsonPropertyOrder({"target", "diffreport" })
 public class DiffReport {
 
     /**
