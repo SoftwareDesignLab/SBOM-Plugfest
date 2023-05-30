e9cfdbc8e39619f1b2884f61ac301ad2cddc6ced
Matthew Morrison <msm8275@rit.edu>
2023-05-30 14:11:58 -0400

    Implementation of ValidHashDataTest. Docstrings for HasHashDataTest.

--- core/src/main/java/org/nvip/plugfest/tooling/qa/processors/UniquenessProcessor.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/processors/UniquenessProcessor.java
@@ -1,13 +1,14 @@
 package org.nvip.plugfest.tooling.qa.processors;
 
 import org.nvip.plugfest.tooling.qa.tests.HasHashDataTest;
+import org.nvip.plugfest.tooling.qa.tests.ValidHashDataTest;
 
 public class UniquenessProcessor extends AttributeProcessor {
 
     public UniquenessProcessor(){
         this.attributeName = "Uniqueness";
         this.metricTests.add(new HasHashDataTest());
-
+        this.metricTests.add(new ValidHashDataTest());
         /*
         TODO
          validHashData: Check to see hash matches schema
