1f1d6d2204333e9dd639dc693162712f11d74e17
Ian Dunn <itd3516@rit.edu>
2023-05-30 16:41:02 -0400

    Update TranslatorPlugFest to throw on invalid filetype

--- core/src/main/java/org/nvip/plugfest/tooling/translator/TranslatorPlugFest.java
+++ core/src/main/java/org/nvip/plugfest/tooling/translator/TranslatorPlugFest.java
@@ -10,6 +10,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 
 /**
@@ -20,6 +22,8 @@ import java.util.Arrays;
  * @author Matt London
  */
 public class TranslatorPlugFest {
+    private final static String INVALID_FILE_CONTENTS = "Invalid SBOM file contents (could not assume schema).";
+    private final static Function<String, String> INVALID_FILE_TYPE = (ext) -> "File type " + ext + " not supported.";
 
     /**
      * Parse an SBOM using the appropriate translator and return the object
@@ -49,41 +53,35 @@ public class TranslatorPlugFest {
      * @throws TranslatorException if translation failed
      */
     public static SBOM translateContents(String contents, String filePath) throws TranslatorException {
-
-        SBOM sbom = null;
-
         try {
             TranslatorCore translator = getTranslator(contents, filePath);
 
-            if (translator == null) throw new TranslatorException("Invalid SBOM file contents (could not assume " +
-                "schema). File: " + filePath);
+            if (translator == null) throw new TranslatorException(INVALID_FILE_CONTENTS + " File: " + filePath);
 
-            sbom = translator.translateContents(contents, filePath);
+            return translator.translateContents(contents, filePath); // TODO test invalid test cases
         }
         catch (Exception e) {
             throw new TranslatorException(e.getMessage());
         }
-
-        return sbom;
     }
 
-    private static TranslatorCore getTranslator(String contents, String filePath) {
+    private static TranslatorCore getTranslator(String contents, String filePath) throws TranslatorException {
         String ext = filePath.substring(filePath.lastIndexOf('.') + 1).trim().toLowerCase();
 
         switch (ext.toLowerCase()) { // TODO possibly scan for an entire file header to ensure validity?
             case "json" -> {
                 if (contents.contains("\"bomFormat\": \"CycloneDX\"")) return new TranslatorCDXJSON();
 //                else if (contents.contains("\"SPDXID\" : \"SPDXRef-DOCUMENT\"")) return new TranslatorSPDXJSON();
-                else return null;
+                else throw new TranslatorException(INVALID_FILE_CONTENTS + " File: " + filePath);
             }
             case "xml" -> {
                 if (contents.contains("<bom xmlns=\"http://cyclonedx.org/schema/bom/")) return new TranslatorCDXXML();
 //                else if (contents.contains("<SPDXID>SPDXRef-DOCUMENT</SPDXID>")) return new TranslatorSPDXXML();
-                else return null;
+                else throw new TranslatorException(INVALID_FILE_CONTENTS + " File: " + filePath);
             }
 //            case "yml" -> { return new TranslatorSPDXYAML(); }
             case "spdx" -> { return new TranslatorSPDX(); }
-            default -> { return null; }
+            default -> { throw new TranslatorException(INVALID_FILE_TYPE.apply(ext)); }
         }
     }
 }
