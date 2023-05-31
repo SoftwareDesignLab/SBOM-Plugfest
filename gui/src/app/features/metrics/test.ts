1d0c6e9a7017b82a38f23c8505812ccc253f3f48
Tina-DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-05-31 13:58:28 -0400

    Update test.ts

--- gui/src/app/features/metrics/test.ts
+++ gui/src/app/features/metrics/test.ts
@@ -1,3 +1,5 @@
+import { identity } from "rxjs";
+
 export interface test {
   uid: string;
   attributeResults: {
@@ -16,10 +18,44 @@ interface AdditionalInfo {
 }
 
 export class QuailtyReport {
-  uid: string;
+  test: test;
+  categories: string[] = [];
+  results: testResult[] = [];
+  identifiers: Set<string> = new Set<string>();
+  componentResults: { [key: string]: testResult[] } = {};
 
   constructor(test: test) {
-    this.uid = test.uid
-    
+    this.test = test;
+    this.categories = Object.keys(this.test.attributeResults);
+    this.categories.forEach(category => {
+      const tests = Object.keys(this.test.attributeResults[category])
+      tests.forEach(test => {
+        this.results = this.test.attributeResults[category][test];
+        // this.identifiers.add(this.test.attributeResults?.['IDENTIFIER']);
+      });
+
+    });
+  }
+
+  getComponentResults() {
+    if (Object.keys(this.componentResults).length === 0) {
+      this.categories.forEach(category => {
+
+      });
+    }
+  }
+
+  getAllIdentifiers() {
+
+  }
+
+  /**
+    filters results by component
+    @param identifier - component identifier
+  */
+  getResultsByComponent(identifier: string): testResult[] {
+
+    return this.results.filter(element => element.additionalInfo?.["IDENTIFIER"] === identifier);
   }
 }
+
