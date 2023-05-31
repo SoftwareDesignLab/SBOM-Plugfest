deb4ff43755e69cb02f2061b9fece9379bfbcc40
dlg1206 <dlg1206@g.rit.edu>
2023-05-31 15:23:12 -0400

    add utility SBOM Generator

--- core/src/test/java/org/nvip/plugfest/tooling/differ/DiffReportTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/differ/DiffReportTest.java
@@ -1,6 +1,9 @@
 package org.nvip.plugfest.tooling.differ;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.nvip.plugfest.tooling.sbom.Component;
 import org.nvip.plugfest.tooling.sbom.DependencyTree;
 import org.nvip.plugfest.tooling.sbom.SBOM;
@@ -9,6 +12,7 @@ import org.nvip.plugfest.tooling.sbom.uids.PURL;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -18,18 +22,11 @@ import static org.junit.jupiter.api.Assertions.fail;
  */
 public class DiffReportTest {
 
-    private static SBOM TEST_SBOM;
 
-    @BeforeAll
-    public static void GenerateTestSBOM(){
-        TEST_SBOM = new SBOM(SBOM.Type.CYCLONE_DX, "1.4", "1", "supplier",
+    private SBOM generateTemplateSBOM() {
+        SBOM s = new SBOM(SBOM.Type.CYCLONE_DX, "1.4", "1", "supplier",
                 "urn:uuid:1b53623d-b96b-4660-8d25-f84b7f617c54", "2023-01-01T02:36:00-05:00",
                 new HashSet<>(), new DependencyTree());
-
-        SBOM test_SBOM = new SBOM(SBOM.Type.CYCLONE_DX, "1.4", "1", "supplier",
-                "urn:uuid:1a11111a-a11a-1111-1a11-a11a1a111a11", "2023-01-01T00:00:00-05:00",
-                new HashSet<>(), new DependencyTree());
-
         try{
             Component test_component_a = new Component(
                     "red", "red_publisher", "1.1.0",
@@ -46,14 +43,33 @@ public class DiffReportTest {
                     Set.of("cpe2.3::test_blue_cpe"), Set.of(new PURL("pkg:bluepackage/blue@1.1.0")), Set.of("random_blue_swid")
             );
 
-            TEST_SBOM.addComponent(null, test_component_b);
-            TEST_SBOM.addComponent(test_component_b.getUUID(), test_component_a);
-            TEST_SBOM.addComponent(test_component_b.getUUID(), test_component_c);
+            s.addComponent(null, test_component_b);
+            s.addComponent(test_component_b.getUUID(), test_component_a);
+            s.addComponent(test_component_b.getUUID(), test_component_c);
         } catch (Exception e){
-            fail("Failed to generate Mock SBOM");
+            fail("Failed to generate SBOM");
         }
+
+
+
+
+        return s;
     }
 
+    ///
+    /// Constructor
+    ///
+
+    @Test
+    @DisplayName("DiffReport Constructor")
+    public void test_diffreport_constructor(){
+        DiffReport d = new DiffReport("TEST_DIFF", generateTemplateSBOM());
+        assertNotNull(d);
+    }
+
+    ///
+    /// Comparison
+    ///
 
 
 }
