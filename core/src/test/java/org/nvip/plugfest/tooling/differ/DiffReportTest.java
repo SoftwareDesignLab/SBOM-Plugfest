95e882df4109c96999d10c9730e0e0abbe784c74
dlg1206 <dlg1206@g.rit.edu>
2023-05-31 16:21:27 -0400

    Diffreport tests and fix major typo in Diffreport

--- core/src/test/java/org/nvip/plugfest/tooling/differ/DiffReportTest.java
+++ core/src/test/java/org/nvip/plugfest/tooling/differ/DiffReportTest.java
@@ -34,24 +34,20 @@ public class DiffReportTest {
             );
 
             Component test_component_b = new Component(
-                    "red", "red_publisher", "1.2.3",
-                    Set.of("cpe2.3::test_red_cpe_123"), Set.of(new PURL("pkg:redpackage/red@1.2.3")), Set.of("random_red_swid123")
-            );
-
-            Component test_component_c = new Component(
                     "blue", "blue_publisher", "1.1.0",
                     Set.of("cpe2.3::test_blue_cpe"), Set.of(new PURL("pkg:bluepackage/blue@1.1.0")), Set.of("random_blue_swid")
             );
 
-            s.addComponent(null, test_component_b);
-            s.addComponent(test_component_b.getUUID(), test_component_a);
-            s.addComponent(test_component_b.getUUID(), test_component_c);
+            HashSet<String> licences = new HashSet<>();
+            licences.add("License_1");
+            test_component_a.setLicenses(licences);
+
+            s.addComponent(null, test_component_a);
+            s.addComponent(test_component_a.getUUID(), test_component_b);
+
         } catch (Exception e){
             fail("Failed to generate SBOM");
         }
-
-
-
 
         return s;
     }
@@ -71,5 +67,130 @@ public class DiffReportTest {
     /// Comparison
     ///
 
+    @Test
+    @DisplayName("Compare same SBOM, different UID")
+    public void test_compare_same_sbom_diff_uid(){
+        DiffReport d = new DiffReport("TEST_DIFF", generateTemplateSBOM());
+        d.compare("OTHER_UID", generateTemplateSBOM());
+        assertNotNull(d);
+    }
 
+    @Test
+    @DisplayName("Compare same SBOM, same UID")
+    public void test_compare_same_same_uid(){
+        DiffReport d = new DiffReport("TEST", generateTemplateSBOM());
+        d.compare("TEST", generateTemplateSBOM());
+        assertNotNull(d);
+    }
+
+    @Test
+    @DisplayName("Compare 3 SBOMs")
+    public void test_compare_three_same_sboms(){
+        DiffReport d = new DiffReport("ONE", generateTemplateSBOM());
+        d.compare("TWO", generateTemplateSBOM());
+        d.compare("THREE", generateTemplateSBOM());
+        assertNotNull(d);
+    }
+
+    @Test
+    @DisplayName("Compare SBOM Conflicts")
+    public void test_compare_sbom_conflicts(){
+        DiffReport d = new DiffReport("ONE", generateTemplateSBOM());
+
+        SBOM s = new SBOM(SBOM.Type.Other, "7", "2", "Foo",
+                "urn:uuid:1b53623d-b96b-4660-8d25-f84b7f617a54", "2002-01-01T02:36:00-05:00",
+                new HashSet<>(), new DependencyTree());
+        try{
+            Component test_component_a = new Component(
+                    "red", "red_publisher", "1.1.0",
+                    Set.of("cpe2.3::test_red_cpe"), Set.of(new PURL("pkg:redpackage/red@1.1.0")), Set.of("random_red_swid")
+            );
+
+            Component test_component_b = new Component(
+                    "blue", "blue_publisher", "1.1.0",
+                    Set.of("cpe2.3::test_blue_cpe"), Set.of(new PURL("pkg:bluepackage/blue@1.1.0")), Set.of("random_blue_swid")
+            );
+
+            HashSet<String> licences = new HashSet<>();
+            licences.add("License_1");
+            test_component_a.setLicenses(licences);
+
+            s.addComponent(null, test_component_a);
+            s.addComponent(test_component_a.getUUID(), test_component_b);
+
+        } catch (Exception e){
+            fail("Failed to generate other SBOM");
+        }
+
+        d.compare("OTHER", s);
+        assertNotNull(d);
+    }
+
+    @Test
+    @DisplayName("Compare No Components")
+    public void test_compare_no_components(){
+        DiffReport d = new DiffReport("ONE", generateTemplateSBOM());
+
+        SBOM s = new SBOM(SBOM.Type.CYCLONE_DX, "1.4", "1", "supplier",
+                "urn:uuid:1b53623d-b96b-4660-8d25-f84b7f617c54", "2023-01-01T02:36:00-05:00",
+                new HashSet<>(), new DependencyTree());
+
+        d.compare("OTHER", s);
+        assertNotNull(d);
+    }
+
+    @Test
+    @DisplayName("Compare Extra Component")
+    public void test_compare_extra_components(){
+        DiffReport d = new DiffReport("ONE", generateTemplateSBOM());
+
+        SBOM s = generateTemplateSBOM();
+
+        try{
+            Component test_component_a = new Component(
+                    "yellow", "yellow_publisher", "1.2.3",
+                    Set.of("cpe2.3::test_yellow_cpe"), Set.of(new PURL("pkg:yellowpackage/blue@1.2.3")), Set.of("random_yellow_swid")
+            );
+
+            s.addComponent(null, test_component_a);
+
+        } catch (Exception e){
+            fail("Failed to generate SBOM");
+        }
+
+        d.compare("OTHER", s);
+        assertNotNull(d);
+    }
+
+    @Test
+    @DisplayName("Compare Component Conflicts")
+    public void test_compare_component_conflicts(){
+
+        DiffReport d = new DiffReport("ONE", generateTemplateSBOM());
+
+        SBOM s = new SBOM(SBOM.Type.CYCLONE_DX, "1.4", "1", "supplier",
+                "urn:uuid:1b53623d-b96b-4660-8d25-f84b7f617c54", "2023-01-01T02:36:00-05:00",
+                new HashSet<>(), new DependencyTree());
+
+        try{
+            Component test_component_a = new Component(
+                    "red", "other", "1.5.0",
+                    Set.of("cpe2.3::other_red_cpe"), Set.of(new PURL("pkg:other/red@1.5.0")), Set.of("rand_red_swid")
+            );
+
+            HashSet<String> licences = new HashSet<>();
+            licences.add("License_2");
+            test_component_a.setLicenses(licences);
+
+            s.addComponent(null, test_component_a);
+
+        } catch (Exception e){
+            fail("Failed to generate other SBOM");
+        }
+
+
+
+        d.compare("OTHER", s);
+        assertNotNull(d);
+    }
 }
