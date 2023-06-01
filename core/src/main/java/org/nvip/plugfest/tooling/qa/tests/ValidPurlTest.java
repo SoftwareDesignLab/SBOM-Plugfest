ee4f0afd1bc00a3c39ded9623febda4fb011a22f
Henry Lu <hyl2415@rit.edu>
2023-05-31 23:57:49 -0400

    work

--- core/src/main/java/org/nvip/plugfest/tooling/qa/tests/ValidPurlTest.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/tests/ValidPurlTest.java
@@ -16,37 +16,28 @@ public class ValidPurlTest extends org.nvip.plugfest.tooling.qa.tests.MetricTest
 
     private Pattern purlRegex;
 
-    /***
-     * Regex for purl
-     */
     public ValidPurlTest() {
         this.purlRegex = Pattern.compile("^pkg:([a-zA-Z][a-zA-Z0-9-~._%]*\\/)+[a-zA-Z][a-zA-Z0-9-~._%]*(@([a-zA-Z0-9-~._%]+))?(\\?(([a-zA-Z][a-zA-Z0-9_.-]*=.+)&)*([a-zA-Z][a-zA-Z0-9-~._%]*=.+))?(#([a-zA-Z0-9-~._%]*\\/)+[a-zA-Z0-9-~._%]*)?", Pattern.MULTILINE);
     }
 
-    /***
-     * Validates the PURL
-     * @param sbom SBOM to test
-     * @return
-     */
     @Override
     public List<Result> test(SBOM sbom) {
         List<Result> results = new ArrayList<>();
+        Set<String> purlStrings = new HashSet<>();
 
         for (Component c : sbom.getAllComponents()) {
             for (PURL p : c.getPurls()) {
-                if (p == null)
-                    continue;
-                Result r;
-                if (this.purlRegex.matcher(p.toString().strip()).matches()) {
-                    r = new Result(TEST_NAME, Result.STATUS.PASS, "Purl Passed");
-                } else {
-                    r = new Result(TEST_NAME, Result.STATUS.FAIL, "Purl Failed");
-                }
-                r.addContext(sbom, "purl");
-                results.add(r);
+                purlStrings.add(p.toString());
             }
         }
 
+        for (String p : purlStrings) {
+            if (!this.purlRegex.matcher(p.strip()).matches()) {
+                results.add(new Result(TEST_NAME, Result.STATUS.FAIL, "TODO: MESSAGE"));
+            } else {
+                results.add(new Result(TEST_NAME, Result.STATUS.PASS, "TODO: MESSAGE"));
+            }
+        }
 
         // return findings
         return results;
