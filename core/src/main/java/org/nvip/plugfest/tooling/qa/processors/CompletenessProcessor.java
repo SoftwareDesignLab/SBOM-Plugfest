5c63515555d20d6994adb6c0bfa6e05e82c766d4
Henry Lu <hyl2415@rit.edu>
2023-05-31 23:57:49 -0400

    work

--- core/src/main/java/org/nvip/plugfest/tooling/qa/processors/CompletenessProcessor.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/processors/CompletenessProcessor.java
@@ -1,6 +1,6 @@
 package org.nvip.plugfest.tooling.qa.processors;
 
-import org.nvip.plugfest.tooling.qa.tests.EmptyOrNullTest;
+import org.nvip.plugfest.tooling.qa.tests.*;
 
 /**
  * file: CompletenessProcessor.java
@@ -16,13 +16,9 @@ public class CompletenessProcessor extends AttributeProcessor{
     public CompletenessProcessor(){
        this.attributeName = "Completeness";
        this.metricTests.add(new EmptyOrNullTest());
-       /*
-        todo
-        minElementFields - Check SBOM for min elements
-        validPurl - Check if purl string is correct
-        validCPE - Check if CPE string is correct
-        validSWID - Check if SWID string is correct
-        hasRelationships - Check to see if a dependency tree was built
-        */
+       this.metricTests.add(new ValidPurlTest());
+       this.metricTests.add(new ValidCPETest());
+       this.metricTests.add(new ValidSWIDTest());
+       this.metricTests.add(new HasRelationshipsTest());
    }
 }
---
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/tests/ValidCPETest.java
@@ -0,0 +1,15 @@
+package org.nvip.plugfest.tooling.qa.tests;
+
+import org.nvip.plugfest.tooling.qa.tests.Result;
+import org.nvip.plugfest.tooling.sbom.SBOM;
+
+import java.util.List;
+
+public class ValidCPETest extends org.nvip.plugfest.tooling.qa.tests.MetricTest {
+    private static final String TEST_NAME = "ValidCPE";
+
+    @Override
+    public List<Result> test(SBOM sbom) {
+        return null;
+    }
+}
