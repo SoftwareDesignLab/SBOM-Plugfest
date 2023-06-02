f4d1df4b513cac5471039152e04cf5aac45c0b9a
dlg1206 <dlg1206@g.rit.edu>
2023-06-02 13:29:40 -0400

    Parse SPDX license table to check for valid name

--- core/src/main/java/org/nvip/plugfest/tooling/qa/tests/ValidSPDXLicenseTest.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/tests/ValidSPDXLicenseTest.java
@@ -1,11 +1,14 @@
 package org.nvip.plugfest.tooling.qa.tests;
 
 
-import org.cyclonedx.model.License;
+import jregex.Matcher;
+import jregex.Pattern;
+import jregex.REFlags;
+import org.apache.commons.io.IOUtils;
 import org.nvip.plugfest.tooling.sbom.Component;
 import org.nvip.plugfest.tooling.sbom.SBOM;
 
-import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
@@ -16,10 +19,14 @@ import java.util.*;
  * Test each component in the given SBOM if the license is an existing
  * valid SPDX License
  * @author Matthew Morrison
+ * @author Derek Garcia
  */
 public class ValidSPDXLicenseTest extends MetricTest{
 
     private static final String TEST_NAME = "ValidSPDXLicense";
+    private static final String SPDX_LICENSE_LIST_URL = "https://spdx.org/licenses/";
+    // Regex to isolate License name table
+    private static final String SPDX_NAME_TABLE_REGEX = "<table class=\"sortable\">[\\s\\S]*?<tbody>([\\s\\S]*)<\\/tbody>";
 
     /**
      * Test all SBOM components for their licenses and if they are a valid SPDX
@@ -54,50 +61,75 @@ public class ValidSPDXLicenseTest extends MetricTest{
      */
     private List<Result> testSPDXLicense(Component c) {
         List<Result> results = new ArrayList<>();
-        Result r;
-        // if no licenses are declared, test automatically fails
+        Result r = null;
 
-        StringBuilder link = new StringBuilder();
-        URL url;
         // for every license id
-        //TODO only held as a string, if ID is not the identifier, this fails
-        // Can also check if the license provides a url as well
+        //TODO only held as a string. A License object should be created
         for(String l : c.getLicenses()){
+            // First attempt to use license as identifier
             try {
-                // build the link with the given license
-                link.append("https://spdx.org/licenses/").append(l)
-                        .append(".html");
-                // set this link as a new URL
-                url = new URL(link.toString());
-                // set up a connection
-                HttpURLConnection huc =
-                        (HttpURLConnection) url.openConnection();
+                // Ping endpoint
+                URL url = new URL(SPDX_LICENSE_LIST_URL + l);
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
                 huc.setRequestMethod("GET");
-                // get the response code from this url
-                int responseCode = huc.getResponseCode();
-                // if the response code is 200 (HTTP is okay), it is a
-                // valid SPDX License
-                if(responseCode == HttpURLConnection.HTTP_OK){
-                    r = new Result(TEST_NAME, Result.STATUS.PASS, "Valid " +
-                            "SPDX License");
-                }
-                // the url was not valid, so it is not a valid SPDX License
-                else{
-                    r = new Result(TEST_NAME, Result.STATUS.FAIL, "Invalid " +
-                            "SPDX License");
-                }
-                r.addContext(c, "license");
-                results.add(r);
-                // reset link string builder
-                link.setLength(0);
+
+                // valid SPDX License Identifier
+                if(huc.getResponseCode() == HttpURLConnection.HTTP_OK)
+                    r = new Result(TEST_NAME, Result.STATUS.PASS, "Valid SPDX License Identifier");
             }
             // an issue with the url connection or link occurred
-            catch(IOException e){
-                r = new Result(TEST_NAME, Result.STATUS.FAIL, "Invalid " +
-                        "SPDX License");
+            catch(Exception e){
+                r = new Result(TEST_NAME, Result.STATUS.FAIL, "Invalid SPDX License Identifier");
+
+            } finally {
+                // update r and add to results
+                assert r != null;
+                r.updateInfo(Result.Context.STRING_VALUE, l);
                 r.addContext(c, "license");
                 results.add(r);
-                link.setLength(0);
+            }
+
+            // Second attempt to use license as name
+            try {
+                // Ping endpoint
+                URL url = new URL(SPDX_LICENSE_LIST_URL);
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+                huc.setRequestMethod("GET");
+
+                // valid SPDX License Identifier
+                if(huc.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    // Get HTML
+                    InputStream in = huc.getInputStream();
+                    String encoding = huc.getContentEncoding();
+                    encoding = encoding == null ? "UTF-8" : encoding;
+                    String html = IOUtils.toString(in, encoding);
+
+                    // Create table regex
+                    Pattern p = new Pattern(SPDX_NAME_TABLE_REGEX, REFlags.MULTILINE);
+                    Matcher m = p.matcher(html);
+
+                    // Search for License name
+                    if(m.find() && m.group(0).contains(">" + l + "<")){
+                        r = new Result(TEST_NAME, Result.STATUS.PASS, "Valid SPDX License Name");
+                    } else {
+                        r = new Result(TEST_NAME, Result.STATUS.FAIL, "Invalid SPDX License Name");
+                    }
+
+                // Issue querying SPDX License page
+                } else {
+                    r = new Result(TEST_NAME, Result.STATUS.ERROR, "Couldn't query " + SPDX_LICENSE_LIST_URL);
+                }
+
+            }
+            // an issue with the url connection or link occurred
+            catch(Exception e){
+                r = new Result(TEST_NAME, Result.STATUS.FAIL, "Invalid SPDX License Identifier");
+
+            } finally {
+                // update r and add to results
+                r.updateInfo(Result.Context.STRING_VALUE, l);
+                r.addContext(c, "license");
+                results.add(r);
             }
         }
 
