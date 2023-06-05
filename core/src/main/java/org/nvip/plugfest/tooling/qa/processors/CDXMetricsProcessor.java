76f8b21564d3477d9c641e3a23bed139f54b2525
Matthew Morrison <msm8275@rit.edu>
2023-06-05 17:27:24 -0400

    Adding todo for CDXMetricsProcessor

--- core/src/main/java/org/nvip/plugfest/tooling/qa/processors/CDXMetricsProcessor.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/processors/CDXMetricsProcessor.java
@@ -18,5 +18,13 @@ public class CDXMetricsProcessor extends AttributeProcessor{
         this.attributeName = "CDXMetrics";
         this.metricTests.add(new HasBomRefTest());
         this.metricTests.add(new HasBomVersionTest());
+
+        /*TODO
+            hasCompositionAggregate: If compositions are present, does it contain an aggregate?
+            hasExternalRefSpecs: If External References are present, do they contain a url and type?
+            hasServicesName: If services are present, does each service have a name?
+            hasDependenciesRef: If dependencies are present, does each dependency contain a ref?
+         */
+
     }
 }
