34b66a7a67ea99dee256b0b0d0a7ecd1878c9c87
Matthew Morrison <msm8275@rit.edu>
2023-06-05 14:05:33 -0400

    Added author in docstrings

--- core/src/main/java/org/nvip/plugfest/tooling/qa/tests/AccurateCPETest.java
+++ core/src/main/java/org/nvip/plugfest/tooling/qa/tests/AccurateCPETest.java
@@ -14,6 +14,7 @@ import java.util.List;
  * Test purls for if they match stored component data
 
  * @author Derek Garcia
+ * @author Matthew Morrison
  */
 public class AccurateCPETest extends MetricTest{
     // the test name for the results
