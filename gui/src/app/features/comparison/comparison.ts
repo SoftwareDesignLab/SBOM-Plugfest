f879c5a3bc624fe2fe6fcf49251ecdef4918841b
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-06-05 10:31:59 -0400

    Updated comparison interface

--- gui/src/app/features/comparison/comparison.ts
+++ gui/src/app/features/comparison/comparison.ts
@@ -1,68 +1,23 @@
-/** Author: Tina DiLorenzo */
-import { SBOM } from "@models/sbom";
+/** @Author Tina DiLorenzo, Justin Jantzi */
 
-/** @TODO */
-// 1. CREATE A CONSTRUCTOR TAKING IN A JSON OBJECT to create comparisons
-// 2. CONVERT JSON
-//    - readonly arrays TO SETS
-//    - convert keys/values to maps
-
-interface SBOMConflict {
-  conflictTypes?:  readonly any[];
-  conflicts?: readonly any[];
-}
-
-interface ComponentConflict {
-  componentA?: attributes | null;
-  componentB?: attributes | null;
-  conflictTypes?: readonly string[];
-  conflicts?: readonly any[];
-}
-
-export interface attributes {
-  uuid?: string | null;
-  name?: string | null;
-  publisher?: string | null;
-  unpackaged?: boolean;
-  cpes?: readonly attributes[] | readonly string[] | readonly [] | null;
-  purls?: readonly attributes[] | attributes | readonly [];
-  swids?: readonly string[] | readonly [];
-  hashes?: readonly string[] | readonly [];
-  uniqueId?: string | null;
-  uniqueIDType?: string | null;
-  children?: readonly string[] | readonly [];
-  version?: string | null;
-  vulnerabilities?: string[] | [];
-  licenses?: readonly string[] | readonly [] | null;
-  conflicts?: any[] | [];
-  componentName?: string | null;
-  appearances?: readonly Number[]
-  componentVersion?: readonly Number[] | readonly [] | string;
-  packageManager?: string | null;
+interface Conflict {
+  type: string;
+  target: string;
+  other: string;
 }
 
 interface DiffReport {
-  sbomConflict?: SBOMConflict;
-  componentConflicts?: readonly ComponentConflict[];
-}
-
-export interface ComponentVersion {
-  componentName: string | null;
-  componentVersion: string | null;
-  cpes: {[key: string]: attributes} | {};
-  purls: {[key: string]: attributes} | {};
-  swids: {[key: string]: attributes} | {};
-  hashes: {[key: string]: attributes} | {};
-  appearances: readonly number[] | readonly []; // number meaning SBOM ID
-}
-
-interface UniqueIdOccurrence {
-  appearances?: readonly number[];
-  uniqueIdType?: string;
+  sbomConflicts: Conflict[];
+  componentConflicts: {
+    [id: string]: {
+      [id: string]: Conflict
+    }
+  };
 }
 
 export interface Comparison {
-  targetSBOM?: SBOM;
-  diffReports: readonly DiffReport[];
-  comparisons: {[key: string]: readonly ComponentVersion[]};
+  targetSBOM: string;
+  diffReport: {
+    [path: string]: DiffReport
+  };
 }
