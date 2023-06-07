11aecc5979756c42212dec17f72b4621856e4ef3
Tina DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-06-07 15:20:14 -0400

    add in shapes

---
+++ gui/src/app/shared/enum/shape.ts
@@ -0,0 +1,13 @@
+enum shape {
+    square = 'square',
+    circle = 'circle',
+    trapezoid = 'trapezoid',
+    parallelogram = 'parallelogram',
+    triangleUp = 'triangle-up',
+    triangleDown = 'triangle-down',
+    triangleLeft = 'triangle-left',
+    triangleRight = 'triangle-right',
+
+}
+
+export default shape;
